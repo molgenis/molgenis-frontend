0a428a3c81f8c1fafc073dca60e750648311a59a
Fleur Kelpin <f.kelpin@umcg.nl>
2019-04-09 12:44:49 +0200

    test(core-ui): reinstate and fix job tests
    For some reason the job component tests were no longer being run. Reinstate and fix them.

--- packages/core-ui/test/modules/react-components/jobs/JobsTest.js
+++ packages/core-ui/test/modules/react-components/jobs/JobsTest.js
@@ -2,36 +2,54 @@ import test from "tape";
 import React from "react";
 import {jobs} from "./jobs";
 import Jobs from "react-components/jobs/Jobs";
+import JobTable from "react-components/jobs/JobTable";
 import sd from "skin-deep";
 
-test('Test if the Jobs component renders running, success and failed jobs correctly', assert => {
-    const tree = sd.shallowRender(Jobs({
-        jobs: jobs
-    }));
+const tree = sd.shallowRender(Jobs({
+    jobs: jobs,
+    onSelect: function() {}
+},JobTable({
+    customColumns: [
+        {
+            th: 'Type', td: function (job) {
+                return job.type
+            }
+        },
+        {
+            th: 'Message', td: function (job) {
+                return job.progressMessage
+            }
+        }]
+})))
+const html = tree.toString()
 
-    assert.equals(tree.toString(),
-        '<div><div class="row"><div class="col-md-12">' +
-        '<div class="panel panel-primary">' +
-        '<div class="panel-heading">Running Jobs</div>' +
-        '<div class="panel-body"><div>' +
-        '<p>TEST job job<div>Test (started by Tape)' +
-        '<div class="progress background-lightgrey"><div class="progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" style="min-width:2em;width:20%;">100/500</div></div></div>' +
-        '<div class="btn-group" role="group">' +
-        '<button type="button" class="btn btn-default">Show details</button>' +
-        '</div>' +
-        '</p></div></div></div></div></div>' +
-        '<div class="row"><div class="col-md-12"><div class="panel panel-primary">' +
-        '<div class="panel-heading">Finished Jobs</div>' +
-        '<div class="panel-body">' +
-        '<table class="table table-striped">' +
-        '<thead><th></th><th>Status</th><th>When</th><th>Duration</th><th>Type</th><th>Message</th><th>Result</th></thead>' +
-        '<tbody>' +
-        '<tr><td><button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button></td>' +
-        '<td>SUCCESS</td><td>Jan 1, 2100, 12:15 - 12:30 AM (in 84 years)</td><td>15m, 0s</td><td>TEST job</td><td>Test (started by Tape)</td><td></td></tr>' +
-        '<tr><td><button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button></td>' +
-        '<td>FAILED</td><td>Jan 1, 2300, 12:15 - 12:30 AM (in 284 years)</td><td>15m, 0s</td><td>TEST job</td><td>Test (started by Tape)</td><td></td></tr>' +
-        '</tbody></table></div></div></div></div></div>',
-        'Jobs component rendered three types of jobs correctly');
+test('Test if the Jobs component renders Running jobs before Finished jobs', assert => {
+    assert.true(html.indexOf('Running Jobs') < html.indexOf('Finished Jobs'), 'Jobs component should render running jobs before finished jobs');
+    assert.end();
+});
 
+test('Test if the Jobs component renders CANCELING jobs with the running jobs', assert => {
+    assert.true(html.indexOf('CANCELING') > html.indexOf('Running Jobs') && html.indexOf('CANCELING') < html.indexOf('Finished Jobs'), 'Jobs component should render canceling job with the running jobs');
+    assert.end();
+});
+
+test('Test if the Jobs component renders RUNNING jobs with the running jobs', assert => {
+    assert.true(html.indexOf('100/500') > html.indexOf('Running Jobs') && html.indexOf('100/500') < html.indexOf('Finished Jobs'), 'Jobs component should render running job with the running jobs');
+    assert.end();
+});
+
+test('Test if the Jobs component renders CANCELED jobs with the finished jobs', assert => {
+    assert.true(html.indexOf('Canceled') > html.indexOf('Finished Jobs'), 'Jobs component should render canceled job with the finished jobs');
+    assert.end();
+});
+
+test('Test if the Jobs component renders SUCCESS jobs with the finished jobs', assert => {
+    assert.true(html.indexOf('Success') > html.indexOf('Finished Jobs'), 'Jobs component should render successful job with the finished jobs');
+    assert.end();
+});
+
+test('Test if the Jobs component renders FAILED jobs with the finished jobs', assert => {
+    const html = tree.toString()
+    assert.true(html.indexOf('Failed') > html.indexOf('Finished Jobs'), 'Jobs component should render failed job with the finished jobs');
     assert.end();
 });
--- packages/core-ui/test/modules/react-components/jobs/jobs.js
+++ packages/core-ui/test/modules/react-components/jobs/jobs.js
@@ -1,42 +1,69 @@
 const job_success = {
-    identifier: "test_1",
-    status: "SUCCESS",
-    type: "TEST job",
-    entityTypeId: "TapeTest",
-    progressMax: 1000,
-    progressInt: 1000,
-    progressMessage: "Test (started by Tape)",
-    submissionDate: "2100-01-01T00:00:00+0100",
-    startDate: "2100-01-01T00:15:00+0100",
-    endDate: "2100-01-01T00:30:00+0100",
-};
+  identifier: 'test_1',
+  status: 'SUCCESS',
+  type: 'TEST job',
+  entityTypeId: 'TapeTest',
+  progressMax: 1000,
+  progressInt: 1000,
+  progressMessage: 'Test (started by Tape)',
+  submissionDate: '2100-01-01T00:00:00+0100',
+  startDate: '2100-01-01T00:15:00+0100',
+  endDate: '2100-01-01T00:30:00+0100',
+}
 
 const job_running = {
-    identifier: "test_2",
-    status: "RUNNING",
-    type: "TEST job",
-    entityTypeId: "TapeTest",
-    progressMax: 500,
-    progressInt: 100,
-    progressMessage: "Test (started by Tape)",
-    submissionDate: "2200-01-01T00:00:00+0100",
-    startDate: "2200-01-01T00:15:00+0100",
-    endDate: "2200-01-01T00:30:00+0100",
+  identifier: 'test_2',
+  status: 'RUNNING',
+  type: 'TEST job',
+  entityTypeId: 'TapeTest',
+  progressMax: 500,
+  progressInt: 100,
+  progressMessage: 'Test (started by Tape)',
+  submissionDate: '2200-01-01T00:00:00+0100',
+  startDate: '2200-01-01T00:15:00+0100',
+  endDate: '2200-01-01T00:30:00+0100',
 }
 
 const job_failed = {
-    identifier: "test_3",
-    status: "FAILED",
-    type: "TEST job",
-    entityTypeId: "TapeTest",
-    progressMax: 200,
-    progressInt: 50,
-    progressMessage: "Test (started by Tape)",
-    submissionDate: "2300-01-01T00:00:00+0100",
-    startDate: "2300-01-01T00:15:00+0100",
-    endDate: "2300-01-01T00:30:00+0100",
+  identifier: 'test_3',
+  status: 'FAILED',
+  type: 'TEST job',
+  entityTypeId: 'TapeTest',
+  progressMax: 200,
+  progressInt: 50,
+  progressMessage: 'Test (started by Tape)',
+  submissionDate: '2300-01-01T00:00:00+0100',
+  startDate: '2300-01-01T00:15:00+0100',
+  endDate: '2300-01-01T00:30:00+0100',
 }
 
-const jobs = [job_success, job_running, job_failed];
+const job_canceling = {
+  '_href': '/api/v2/sys_job_ScriptJobExecution/aaaac2p5c3hauascvqkaabaaae',
+  'name': 'test',
+  'parameters': '{"sleep_cycles":"10","sleep_time":"10"}',
+  'identifier': 'aaaac2p5c3hauascvqkaabaaae',
+  'user': 'admin',
+  'status': 'CANCELING',
+  'type': 'Script',
+  'submissionDate': '2019-04-08T13:15:24Z',
+  'startDate': '2019-04-08T13:15:24Z',
+  'log': '13:15:24.301 - Execution started.\n13:15:28.288 - Canceling ...\n'
+}
 
-export {job_failed, job_running, job_success, jobs}
+const job_canceled = {
+  '_href': '/api/v2/sys_job_ScriptJobExecution/aaaac2p5c3hauascvqkaabaaae',
+  'name': 'test',
+  'parameters': '{"sleep_cycles":"10","sleep_time":"10"}',
+  'identifier': 'aaaac2p5c3hauascvqkaabaaae',
+  'user': 'admin',
+  'status': 'CANCELED',
+  'type': 'Script',
+  'submissionDate': '2019-04-08T13:15:24Z',
+  'startDate': '2019-04-08T13:15:24Z',
+  'endDate': '2019-04-08T13:17:05Z',
+  'log': '13:15:24.301 - Execution started.\n13:15:28.288 - Canceling ...\n13:17:05.056 - Canceled\n'
+}
+
+const jobs = [job_success, job_running, job_failed, job_canceling, job_canceled]
+
+export { job_failed, job_running, job_success, job_canceling, job_canceled, jobs }
