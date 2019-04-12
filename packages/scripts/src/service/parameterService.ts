88c8a35bcce8e20a56dda8c6ed1ea25cc87ef81a
connor <c.stroomberg@gmail.com>
2019-04-12 12:38:35 +0200

    improve param finder

--- packages/scripts/src/service/parameterService.ts
+++ packages/scripts/src/service/parameterService.ts
@@ -1,14 +1,25 @@
-const freeMarkerRegEx = /\${(\w+)}/g // Match foo from ${foo}
-const magmaRegEx = /\$\(\"(\w+)\"\)/g // Match foo from $('foo')
+const freeMarkerRegEx = /\${([a-zA-Z0-9-#]+)}/g // Match foo from ${foo}
+const magmaRegEx = /\$\(\"([a-zA-Z0-9-#]+)\"\)|$\(\'([a-zA-Z0-9-#]+)\'\)/g // Match foo from $('foo') or $("foo")
+const validFirstChar = /[a-zA-Z]/ // only chars are allowed at the start 
 
-const findParameters = (codeString: string, type: string): string[] => {
-  const parameterRegEx = type === 'JavaScript (Magma)' ? magmaRegEx : freeMarkerRegEx
+const isNotReservedWord = (word: string) => !["login", "logout", "csv", "base", "exist", "meta", "_idValue"].includes(word)
+const isNotStartingWithChar = (word: string) => validFirstChar.test(word.charAt(0))
+
+const extraxtCandidates = (codeString: string, regEx: RegExp) => {
   let result
   let list = []
-  while ((result = parameterRegEx.exec(codeString)) !== null) {
+  while ((result = regEx.exec(codeString)) !== null) {
     list.push(result[1]) // add the capture group
   }
   return list
+}
+
+const findParameters = (codeString: string, type: string): string[] => {
+  const parameterRegEx = type === 'JavaScript (Magma)' ? magmaRegEx : freeMarkerRegEx
+  const candidates = extraxtCandidates(codeString, parameterRegEx)
+
+  const filtered = candidates.filter(isNotReservedWord).filter(isNotStartingWithChar)
+  return filtered
 }
 
 export {
--- packages/scripts/tests/unit/service/parameterService.spec.ts
+++ packages/scripts/tests/unit/service/parameterService.spec.ts
@@ -1,17 +1,29 @@
 import { findParameters } from '@/service/parameterService'
 
 describe('service/parameterService', () => {
-    describe('findParameters', () => {
-        it('Find non magma parameter', () => {
+    describe('FindParameters for non Magma types (freemarker style)', () => {
+        it('should find \'koe\' in \'bla bla bla ${koe} bla bla\'', () => {
             expect(findParameters('bla bla bla ${koe} bla bla', 'test')).toEqual(['koe'])
         })
+    })
 
-        it('Find magma parameter', () => {
+    describe('FindParameters for Magma script', () => {
+        it('should find \'myValue\' in \'bla bla bla $("myValue") bla bla\'', () => {
             expect(findParameters('bla bla bla $("myValue") bla bla', 'JavaScript (Magma)')).toEqual(['myValue'])
         })
 
-        it('should not find params with single quotes in when type is JavaScript (Magma)', () => {
-            expect(findParameters("$('blah-di-blah')", 'JavaScript (Magma)')).toEqual([])
+        it('should find params with single quotes in when type is JavaScript (Magma)', () => {
+            expect(findParameters('$("blah-di-blah")', 'JavaScript (Magma)')).toEqual(['blah-di-blah'])
+        })
+    })
+
+    describe('FindParameters', () => {
+        it('should find not \'3demo\' in \'${3demo}\'', () => {
+            expect(findParameters('${3demo}', 'test')).toEqual([])
+        })
+
+        it('should find not \'base\' in \'${base}\' as \'base\' is a reserved key word ', () => {
+            expect(findParameters('${base}', 'test')).toEqual([])
         })
     })
 })
