5ec6cd420a869772919314e38f91fb40dd9d8b3b
MaxPostema <max@elynor.com>
2019-07-19 16:50:43 +0200

    choir: move jest config to external file

---
+++ packages/data-explorer/jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue',
+    'ts',
+    'tsx'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.es.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@molgenis/molgenis-api-client.*))'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  },
+  collectCoverage: true
+}
