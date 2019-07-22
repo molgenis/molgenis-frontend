c51840a5219c6497b21b5c718523acecdfdd7c89
sidohaakma <sido@haakma.org>
2019-07-22 08:34:20 +0200

    build(copy-resources): change directory to preview-config

--- docker/preview-config/copy_package_dist_dirs.sh
+++ docker/preview-config/copy_package_dist_dirs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Copies the package dist dirs to the docker context so that they can be copied to container.
-rm -rf docker/preview/dist
-mkdir -p docker/preview/dist/@molgenis-ui
+rm -rf docker/preview-config/dist
+mkdir -p docker/preview-config/dist/@molgenis-ui
 for file in packages/*; do 
-    mkdir -p "docker/preview/dist/@molgenis-ui/$(basename "$file")/dist"
-    cp -rf "${file}/dist" "docker/preview/dist/@molgenis-ui/$(basename "$file")"
+    mkdir -p "docker/preview-config/dist/@molgenis-ui/$(basename "$file")/dist"
+    cp -rf "${file}/dist" "docker/preview-config/dist/@molgenis-ui/$(basename "$file")"
 done
