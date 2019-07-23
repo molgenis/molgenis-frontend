16f74573bb7ef6cd005568b5417661e3d6112202
MaxPostema <max@elynor.com>
2019-07-23 10:08:55 +0200

    Fixed tests

--- packages/data-explorer/jest.config.js
+++ packages/data-explorer/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     '^.+\\.es.js$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@molgenis/molgenis-api-client.*))'
+    '/node_modules/(?!(@molgenis/*))'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
