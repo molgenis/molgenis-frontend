7888af6a7520e9d04d4e818e3a2af92483a86539
MaxPostema <max@elynor.com>
2019-08-09 14:20:23 +0200

    Added dummy getter test

--- packages/data-explorer/tests/unit/store/getters.spec.ts
+++ packages/data-explorer/tests/unit/store/getters.spec.ts
@@ -1,7 +1,12 @@
 import getters from '@/store/getters'
+import state from '@/store/state'
 
 describe('getters', () => {
   it('has activeEntityData', () => {
     expect(getters.activeEntityData).toBeDefined()
+  })
+
+  it('should be false is no search term is given', () => {
+    expect(getters.activeEntityData({ ...state })).toEqual(null)
   })
 })
