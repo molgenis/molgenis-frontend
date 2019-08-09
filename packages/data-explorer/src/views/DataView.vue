57c938709e4619f0ef8aaad9ba425b2001f06a60
mkslofstra <mariska-karina@hotmail.com>
2019-08-09 10:11:26 +0200

    refactor(DataView): Move EntityView to seperate component

--- packages/data-explorer/src/views/DataView.vue
+++ packages/data-explorer/src/views/DataView.vue
@@ -1,34 +1,21 @@
 <template>
   <div>
     <toolbar-view />
-    <entity-cards
-      :entities="activeEntityData"
-      :isShop="true"
-      v-if="dataDisplayLayout === 'cards'" />
-    <entity-table
-      :entities="activeEntityData"
-      :isShop="true"
-      v-else />
+    <entity-view />
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import { mapGetters, mapState } from 'vuex'
 import ToolbarView from './ToolbarView'
-import EntityCards from '../components/DataView/EntityCards'
-import EntityTable from '../components/DataView/EntityTable'
+import EntityView from './EntityView'
 
 export default Vue.extend({
   name: 'DataView',
-  computed: {
-    ...mapGetters(['activeEntityData']),
-    ...mapState(['dataDisplayLayout'])
-  },
   created () {
     this.$store.dispatch('loadEntity')
     this.$store.dispatch('loadMetaData')
   },
-  components: { ToolbarView, EntityCards, EntityTable }
+  components: { ToolbarView, EntityView }
 })
 </script>
---
+++ packages/data-explorer/src/views/EntityView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <entity-cards
+      :entities="activeEntityData"
+      :isShop="true"
+      v-if="dataDisplayLayout === 'cards'"/>
+    <entity-table
+      :entities="activeEntityData"
+      :isShop="true"
+      v-else/>
+  </div>
+</template>
+
+<script>
+import EntityCards from '../components/DataView/EntityCards'
+import EntityTable from '../components/DataView/EntityTable'
+import { mapGetters, mapState } from 'vuex'
+
+export default {
+  name: 'EntityView',
+  components: { EntityCards, EntityTable },
+  computed: {
+    ...mapGetters(['activeEntityData']),
+    ...mapState(['dataDisplayLayout'])
+  }
+}
+</script>
