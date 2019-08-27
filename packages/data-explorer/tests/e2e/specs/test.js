5b7ee5b646de783a814eaa623a37897fb5207bf6
MaxPostema <max@elynor.com>
2019-08-27 11:09:19 +0200

    feat(data-explorer): Add e2e test

--- packages/data-explorer/tests/e2e/specs/test.js
+++ packages/data-explorer/tests/e2e/specs/test.js
@@ -5,8 +5,16 @@ module.exports = {
   'default e2e tests': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.containsText('h2', 'Data-explorer')
+      .waitForElementVisible('#app', 50000)
+      .assert.containsText('h2', 'Filters')
+      .end()
+  },
+  'open/close filters': browser => {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible('#app', 50000)
+      .assert.con
+      .click('.btn .hide-filters')
       .end()
   }
 }
