e3aaa765e1eda38f21f681d2a0a839d676420182
connor <c.stroomberg@gmail.com>
2019-08-28 12:31:41 +0200

    task(data-explorer): improve performance, fetch metaData without data.

--- packages/data-explorer/src/repository/metaDataRepository.ts
+++ packages/data-explorer/src/repository/metaDataRepository.ts
@@ -3,7 +3,7 @@ import api from '@molgenis/molgenis-api-client'
 
 // Todo placeholder until we have a metadataApi
 const fetchMetaData = async (entityId: string) => {
-  const resp = await api.get(`/api/v2/${entityId}`)
+  const resp = await api.get(`/api/v2/${entityId}?num=0`)
   return resp.meta
 }
 
