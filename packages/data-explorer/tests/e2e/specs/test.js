c7d1e6fd8addc63b0f9df39fa12920c73454b9dd
MaxPostema <max@elynor.com>
2019-08-28 09:40:45 +0200

    Added consts and removed superfluous delays

--- packages/data-explorer/tests/e2e/specs/test.js
+++ packages/data-explorer/tests/e2e/specs/test.js
@@ -1,69 +1,62 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
+const timeOutDelay = 5000
+const animationDelay = 300
 
 module.exports = {
   'show table layout and card layout': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .click('.jumbotron .btn.btn-primary.btn-lg')
-      .pause(300)
-      .waitForElementVisible('.card-columns', 5000)
+      .pause(animationDelay)
+      .waitForElementVisible('.card-columns', timeOutDelay)
       .click('.table-layout')
-      .waitForElementVisible('table.table', 5000)
+      .waitForElementVisible('table.table', timeOutDelay)
       .click('.card-layout')
-      .waitForElementVisible('.card-columns', 5000)
+      .waitForElementVisible('.card-columns', timeOutDelay)
       .end()
   },
   'add and remove items from shoppingcart': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
+      .waitForElementVisible('#app', timeOutDelay)
       .click('.jumbotron .btn.btn-primary.btn-lg')
-      .pause(300)
-      .waitForElementVisible('.shopping-button', 5000)
+      .pause(animationDelay)
+      .waitForElementVisible('.shopping-button', timeOutDelay)
       .click('.shopping-button')
-      .pause(300)
       .click('.show-cart')
-      .waitForElementVisible('.cart-order', 5000)
+      .waitForElementVisible('.cart-order', timeOutDelay)
       .assert.elementNotPresent('.alert.alert-warningn')
       .click('.shopping-button')
-      .pause(300)
-      .waitForElementVisible('.alert.alert-warning', 5000)
+      .waitForElementVisible('.alert.alert-warning', timeOutDelay)
       .end()
   },
   'Add and remove filters': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .click('.jumbotron .btn.btn-primary.btn-lg')
-      .pause(300)
+      .pause(animationDelay)
       .click('.remove-button')
-      .pause(300)
       .click('.remove-button')
-      .pause(300)
       .click('.add-button')
-      .pause(300)
       .click('.btn.btn-primary')
-      .pause(300)
       .click('.add-button')
-      .pause(300)
       .click('.btn.btn-primary')
       .end()
   },
   'open/close filters sidebar': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
+      .waitForElementVisible('#app', timeOutDelay)
       .click('.jumbotron .btn.btn-primary.btn-lg')
-      .pause(300)
-      .waitForElementVisible('.btn.hide-filters', 5000)
+      .pause(animationDelay)
+      .waitForElementVisible('.btn.hide-filters', timeOutDelay)
       .assert.elementNotPresent('show-filters-button')
       .assert.cssClassNotPresent('.flex-mainview', 'hidefilters')
       .click('.btn.hide-filters')
-      .pause(300)
       .assert.cssClassPresent('.flex-mainview', 'hidefilters')
-      .waitForElementVisible('.btn.show-filters-button', 5000)
+      .waitForElementVisible('.btn.show-filters-button', timeOutDelay)
       .click('.btn.show-filters-button')
-      .pause(300)
       .assert.elementNotPresent('show-filters-button')
       .assert.cssClassNotPresent('.flex-mainview', 'hidefilters')
       .end()
