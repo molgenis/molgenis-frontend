e8ede3c0044bafcd02d670ae2e5a8e63099349bc
MaxPostema <max@elynor.com>
2019-08-28 11:46:31 +0200

    Fixed safari e2e test

--- packages/data-explorer/tests/e2e/specs/test.js
+++ packages/data-explorer/tests/e2e/specs/test.js
@@ -31,19 +31,6 @@ module.exports = {
       .waitForElementVisible('.alert.alert-warning', timeOutDelay)
       .end()
   },
-  'Add and remove filters': browser => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .click('.jumbotron .btn.btn-primary.btn-lg')
-      .pause(animationDelay)
-      .click('.remove-button')
-      .click('.remove-button')
-      .click('.add-button')
-      .click('.btn.btn-primary')
-      .click('.add-button')
-      .click('.btn.btn-primary')
-      .end()
-  },
   'open/close filters sidebar': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
@@ -60,5 +47,19 @@ module.exports = {
       .assert.elementNotPresent('show-filters-button')
       .assert.cssClassNotPresent('.flex-mainview', 'hidefilters')
       .end()
+  },
+  'Add and remove filters': browser => {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible('#app', timeOutDelay)
+      .click('.jumbotron .btn.btn-primary.btn-lg')
+      .pause(animationDelay)
+      .expect.elements('.filter-container .filter-card').count.to.equal(4)
+    browser.click('.remove-button')
+      .expect.elements('.filter-container .filter-card').count.to.equal(3)
+    browser.click('.add-button')
+      .assert.elementPresent('#modal-add-filter')
+      .click('.btn.btn-primary')
+    browser.end()
   }
 }
