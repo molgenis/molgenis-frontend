72fe5e1f5f6b8ef919994c16eb5f0aea5bef726f
MaxPostema <max@elynor.com>
2019-09-03 13:54:49 +0200

    get full record data

--- packages/data-explorer/src/components/dataView/CustomCardContent.vue
+++ packages/data-explorer/src/components/dataView/CustomCardContent.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="custom-card position-relative">
     <v-runtime-template :template="template"></v-runtime-template>
+
     <pre style="border:1px solid red">
     {{customCode}}
-      ---
-      {{record}}
+    ---
+    {{record}}
     </pre>
+
   </div>
 </template>
 
 <script>
 import VRuntimeTemplate from 'v-runtime-template'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'CustomCardContent',
   components: { VRuntimeTemplate },
   props: {
+    id: {
+      type: String,
+      required: true
+    },
     record: {
       type: Object,
       required: true
@@ -24,6 +31,12 @@ export default {
       type: String,
       required: true
     }
+  },
+  methods: {
+    ...mapActions(['fetchRowData'])
+  },
+  created () {
+    this.fetchRowData({ rowId: this.id })
   },
   computed: {
     template: function () {
