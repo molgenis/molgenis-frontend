10822076162286562fc6b4044816d0a1fa32e490
MaxPostema <max@elynor.com>
2019-09-03 12:29:28 +0200

    Add vue template

--- packages/data-explorer/src/components/dataView/CustomCardContent.vue
+++ packages/data-explorer/src/components/dataView/CustomCardContent.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="custom-card position-relative">
-    <div v-html="customCode"></div>
-    <pre>
-    |{{customCode}}|
+    <v-runtime-template :template="template"></v-runtime-template>
+    <pre style="border:1px solid red">
+    {{customCode}}
+      ---
+      {{record}}
     </pre>
   </div>
 </template>
 
 <script>
+import VRuntimeTemplate from 'v-runtime-template'
+
 export default {
   name: 'CustomCardContent',
+  components: { VRuntimeTemplate },
   props: {
+    record: {
+      type: Object,
+      required: true
+    },
     customCode: {
       type: String,
       required: true
+    }
+  },
+  computed: {
+    template: function () {
+      return '<div>' + this.customCode.trim() + '</div>'
     }
   }
 }
