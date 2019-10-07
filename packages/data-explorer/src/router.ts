fb1a1f3c85ff73f6f95c17692cc16e9d03413eac
Fleur Kelpin <f.kelpin@umcg.nl>
2019-10-07 16:05:03 +0200

    fix(data-explorer): no history mode for router

--- packages/data-explorer/src/router.ts
+++ packages/data-explorer/src/router.ts
@@ -7,7 +7,6 @@ Vue.use(Router)
 const packageJson = require('../package.json')
 
 export default new Router({
-  mode: 'history',
   base: process.env.NODE_ENV === 'production' ? packageJson.name : process.env.BASE_URL,
   routes: [
     {
