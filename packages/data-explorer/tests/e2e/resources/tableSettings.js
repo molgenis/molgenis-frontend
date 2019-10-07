223817e1bdc7f7f2e8215b8edd7b9ab934f70790
connoratrug <47183404+connoratrug@users.noreply.github.com>
2019-10-07 19:59:35 +0200

    test(data-explorer): Refactor test data to human readable format (#153)

---
+++ packages/data-explorer/tests/e2e/resources/tableSettings.js
@@ -0,0 +1,28 @@
+module.exports = {
+    links: {
+      self: 'http://data-v3.dev.molgenis.org:443/api/data/de_dataexplorer_table_settings'
+    },
+    items: [
+      {
+        links: {
+          self: 'http://data-v3.dev.molgenis.org:443/api/data/de_dataexplorer_table_settings/aaaac3fzdyjklywoophjs5yaai'
+        },
+        data: {
+          id: 'aaaac3fzdyjklywoophjs5yaai',
+          table: {
+            links: {
+              self: 'http://data-v3.dev.molgenis.org:443/api/data/sys_md_EntityType/it_emx_datatypes_TypeTest'
+            }
+          },
+          table_label: 'TypeTest',
+          shop: true
+        }
+      }
+    ],
+    page: {
+      size: 1,
+      totalElements: 1,
+      totalPages: 1,
+      number: 0
+    }
+  }
