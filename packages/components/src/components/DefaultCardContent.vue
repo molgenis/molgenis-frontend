adb10dd3f652e405791438af7d6725a508b468ab
connor <c.stroomberg@gmail.com>
2019-10-09 15:25:26 +0200

    task(data-explorer, components): move DefaultCardContent to components module

--- packages/components/src/components/DefaultCardContent.vue
+++ packages/components/src/components/DefaultCardContent.vue
@@ -26,7 +26,6 @@
 
 <script>
 
-
 export default {
   name: 'DefaultCardContent',
   data: () => {
--- packages/data-explorer/src/components/dataView/ExplorerCard.vue
+++ packages/data-explorer/src/components/dataView/ExplorerCard.vue
@@ -32,7 +32,7 @@
 <script>
 import Vue from 'vue'
 import ShoppingButton from '../utils/ShoppingButton'
-import DefaultCardContent from './DefaultCardContent'
+import DefaultCardContent from '../../../../components/src/components/DefaultCardContent.vue'
 import CustomCardContent from './CustomCardContent'
 
 export default Vue.extend({
