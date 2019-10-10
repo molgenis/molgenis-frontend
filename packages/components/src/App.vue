241972414021ce46bd19b83dea16e34883529f37
connor <c.stroomberg@gmail.com>
2019-10-10 13:03:52 +0200

    feat(components): DefaultCardContent component

--- packages/components/src/App.vue
+++ packages/components/src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Components</h1>
+    <hr/>
+    <h3>Default card</h3>
+    <default-card-content
+    dataLabel="Data label"
+    dataId="dataId"
+    dataTable="dataTable"
+    :dataContents=dataContents
+    :numberOfAttributes="2" ></default-card-content>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import DefaultCardContent from './components/DefaultCardContent.vue'
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld
+    DefaultCardContent
+  },
+  data: () => {
+    return {
+      dataContents: {
+        field1: 'value 1',
+        field2: 2,
+        field3: true
+      }
+    }
   }
 })
 </script>
