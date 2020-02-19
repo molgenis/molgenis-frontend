f545687956882c46588de830362767f267f2c147
connoratrug <c.stroomberg@rug.nl>
2020-02-19 21:48:44 +0100

    fix: metadata cache; make non blocking

--- packages/data-explorer/src/repository/metaDataRepository.ts
+++ packages/data-explorer/src/repository/metaDataRepository.ts
@@ -5,23 +5,39 @@ import { ResponseEntityType } from '../types/EntityTypeV3'
 import { MetaData } from '../types/MetaData'
 
 const metaDataCache:{ [s: string]: MetaData } = {}
+const metaDataCue: { [s: string]: Promise<AxiosResponse<ResponseEntityType>> } = {}
 
-const fetchMetaDataById = async (entityId: string): Promise<MetaData> => {
-  if (metaDataCache[entityId]) {
-    return metaDataCache[entityId]
-  }
-
-  const response = await axios.get<ResponseEntityType>(`/api/metadata/${entityId}`, {
-    params: {
-      flattenAttributes: true
-    }
-  })
+const mapAndStore = (entityId:string, response: AxiosResponse<ResponseEntityType>) => {
   const entityType = response.data
   const metadata = mapper.toMetaData(entityType)
 
   // Cache mapped response to improve speed
   metaDataCache[entityId] = metadata
   return metadata
+}
+
+const fetchMetaDataById = async (entityId: string): Promise<MetaData> => {
+  if (metaDataCache[entityId]) {
+    return metaDataCache[entityId]
+  }
+  if (metaDataCue[entityId]) {
+    return metaDataCue[entityId].then((response) => {
+      if (metaDataCache[entityId]) {
+        return metaDataCache[entityId]
+      }
+      return mapAndStore(entityId, response)
+    })
+  }
+
+  const response = axios.get<ResponseEntityType>(`/api/metadata/${entityId}`, {
+    params: {
+      flattenAttributes: true
+    }
+  })
+
+  metaDataCue[entityId] = response
+  const resloved = await response
+  return mapAndStore(entityId, resloved)
 }
 
 const fetchMetaDataByURL = async (url: string): Promise<MetaData> => {
