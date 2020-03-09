78a9a2bdcc08843e959f57b837bd3ac32689ae62
Jelmer Veen <j.p.veen@rug.nl>
2020-03-09 16:25:32 +0100

    fix: added more testdata

--- packages/data-explorer/tests/unit/mocks/metaDataV3ResponseMock.ts
+++ packages/data-explorer/tests/unit/mocks/metaDataV3ResponseMock.ts
@@ -35,6 +35,47 @@ export default {
             aggregatable: false,
             defaultValue: 'default'
           }
+        },
+        {
+          link: {
+            self: 'https://server/api/metadata/mock-entity/attributes/mock-entity-attribute2'
+          },
+          data: {
+            id: 'mock-entity-attribute2',
+            name: 'id',
+            sequenceNr: 0,
+            type: 'string',
+            idAttribute: true,
+            labelAttribute: true,
+            nullable: false,
+            auto: false,
+            visible: true,
+            unique: true,
+            readOnly: true,
+            aggregatable: false,
+            defaultValue: 'default',
+            parentAttribute: {
+              link: {
+                self: 'https://server/api/metadata/mock-entity/attributes/mock-entity-attribute'
+              },
+              data: {
+                id: 'mock-entity-attribute',
+                label: 'id',
+                name: 'id',
+                sequenceNr: 0,
+                type: 'one_to_many',
+                idAttribute: true,
+                labelAttribute: false,
+                nullable: false,
+                auto: false,
+                visible: true,
+                unique: true,
+                readOnly: true,
+                aggregatable: false,
+                defaultValue: 'default'
+              }
+            }
+          }
         }
       ]
     },
