3aee35e006ddf8cd71f9c7d3c52f23a143fa3971
connoratrug <c.stroomberg@rug.nl>
2020-04-01 15:49:44 +0200

    comment back in unit tests for clibboard

--- packages/data-explorer/tests/unit/views/ClipboardView.spec.ts
+++ packages/data-explorer/tests/unit/views/ClipboardView.spec.ts
@@ -63,25 +63,25 @@ describe('ClipboardView.vue', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
 
-  // it('closes shoppingcart', () => {
-  //   const wrapper = shallowMount(ClipboardView, { store, localVue })
-  //   const button = wrapper.find('button.cart-back')
-  //   button.trigger('click')
-  //   expect(mutations.setShowShoppingCart).toHaveBeenCalledWith(state, false)
-  //   expect(mutations.setHideFilters).toHaveBeenCalledWith(state, false)
-  // })
+  it('closes shoppingcart', () => {
+    const wrapper = shallowMount(ClipboardView, { store, localVue })
+    const button = wrapper.find('button.cart-back')
+    button.trigger('click')
+    expect(mutations.setShowShoppingCart).toHaveBeenCalledWith(state, false)
+    expect(mutations.setHideFilters).toHaveBeenCalledWith(state, false)
+  })
 
-  // it('returns true for selected shopping item', () => {
-  //   const wrapper = shallowMount(ClipboardView, { store, localVue })
-  //   const entity = { tableID: '1', label: 'blaat1' }
-  //   // @ts-ignore
-  //   expect(wrapper.vm.isSelected(entity)).toBe(true)
-  // })
+  it('returns true for selected shopping item', () => {
+    const wrapper = shallowMount(ClipboardView, { store, localVue })
+    const entity = { tableID: '1', label: 'blaat1' }
+    // @ts-ignore
+    expect(wrapper.vm.isSelected(entity)).toBe(true)
+  })
 
-  // it('returns false for item that is not selected', () => {
-  //   const wrapper = shallowMount(ClipboardView, { store, localVue })
-  //   const entity = { tableID: '2', label: 'blaat2' }
-  //   // @ts-ignore
-  //   expect(wrapper.vm.isSelected(entity)).toBe(false)
-  // })
+  it('returns false for item that is not selected', () => {
+    const wrapper = shallowMount(ClipboardView, { store, localVue })
+    const entity = { tableID: '2', label: 'blaat2' }
+    // @ts-ignore
+    expect(wrapper.vm.isSelected(entity)).toBe(false)
+  })
 })
