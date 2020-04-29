ad36c9bedbfc5dacebee29cc86474539104e1461
Max Postema <m.e.postema@rug.nl>
2020-04-29 16:38:23 +0200

    fix: make unit test work again

--- packages/data-explorer/src/store/getters.ts
+++ packages/data-explorer/src/store/getters.ts
@@ -2,8 +2,9 @@ import ApplicationState from '@/types/ApplicationState'
 import { createRSQLQuery } from '@/mappers/rsqlMapper'
 
 export default {
-  filterRsql: (state: ApplicationState): string | null =>
-    state.tableMeta && createRSQLQuery(state.filters, state.searchText),
+  filterRsql: (state: ApplicationState): string | null => {
+    return state.tableMeta && createRSQLQuery(state.filters, state.searchText)
+  },
   hasManagerRole: (state: ApplicationState) => {
     console.log(state)
     return state.context.context.roles.includes('ROLE_MANAGER')
