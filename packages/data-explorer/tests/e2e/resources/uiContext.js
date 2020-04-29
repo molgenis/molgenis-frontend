0771fdc1ae73721a00a49db09716cf25196928ae
Max Postema <m.e.postema@rug.nl>
2020-04-29 19:38:03 +0200

    test: fix e2e tests

--- packages/data-explorer/tests/e2e/resources/uiContext.js
+++ packages/data-explorer/tests/e2e/resources/uiContext.js
@@ -1,170 +1,67 @@
 module.exports = {
-  logoTopMaxHeight: 150,
-  navBarLogo: '/img/Logo_Blue_Small.png',
-  menu: {
-    type: 'menu',
-    id: 'main',
-    label: 'Home',
-    items: [
+  'logoTopMaxHeight': 150,
+  'navBarLogo': '/img/Logo_Blue_Small.png',
+  'menu': {
+    'id': 'main',
+    'label': 'Home',
+    'items': [
       {
-        type: 'plugin',
-        id: 'home',
-        label: 'Home'
+        'type': 'plugin',
+        'id': 'home',
+        'label': 'Home'
       },
       {
-        type: 'menu',
-        id: 'importdata',
-        label: 'Import data',
-        items: [
+        'type': 'plugin',
+        'id': 'redirect',
+        'label': 'Bioinformatics',
+        'params': 'url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBioinformatics'
+      },
+      {
+        'type': 'menu',
+        'id': 'experimental',
+        'label': 'Experimental',
+        'items': [
           {
-            type: 'plugin',
-            id: 'one-click-importer',
-            label: 'Quick data import'
+            'type': 'plugin',
+            'id': 'redirect',
+            'label': 'DS2 Books',
+            'params': 'url=%2F%40molgenis-experimental%2Fdata-explorer%2Fdist%2Findex.html%23%2Fquest_nillable_Books'
           },
           {
-            type: 'plugin',
-            id: 'importwizard',
-            label: 'Advanced data import'
+            'type': 'plugin',
+            'id': 'redirect',
+            'label': 'Collections',
+            'params': 'url=%2F%40molgenis-experimental%2Fdata-explorer%2Fdist%2Findex.html%23%2Feu_bbmri_eric_collections'
+          },
+          {
+            'type': 'plugin',
+            'id': 'redirect',
+            'label': 'Biobanks',
+            'params': 'url=%2F%40molgenis-experimental%2Fdata-explorer%2Fdist%2Findex.html%23%2Feu_bbmri_eric_biobanks'
+          },
+          {
+            'type': 'plugin',
+            'id': 'redirect',
+            'label': 'TypeTest',
+            'params': 'url=%2F%40molgenis-experimental%2Fdata-explorer%2Fdist%2Findex.html%23%2Ftest_TypeTest'
           }
         ]
-      },
-      {
-        type: 'plugin',
-        id: 'navigator',
-        label: 'Navigator'
-      },
-      {
-        type: 'plugin',
-        id: 'dataexplorer',
-        label: 'Data Explorer'
-      },
-      {
-        type: 'menu',
-        id: 'dataintegration',
-        label: 'Data Integration',
-        items: [
-          {
-            type: 'plugin',
-            id: 'metadata-manager',
-            label: 'Metadata Manager'
-          },
-          {
-            type: 'plugin',
-            id: 'mappingservice',
-            label: 'Mapping Service'
-          },
-          {
-            type: 'plugin',
-            id: 'sorta',
-            label: 'SORTA'
-          },
-          {
-            type: 'plugin',
-            id: 'tagwizard',
-            label: 'Tag Wizard'
-          }
-        ]
-      },
-      {
-        type: 'menu',
-        id: 'plugins',
-        label: 'Plugins',
-        items: [
-          {
-            type: 'plugin',
-            id: 'searchAll',
-            label: 'Search all data'
-          },
-          {
-            type: 'plugin',
-            id: 'swagger',
-            label: 'API documentation'
-          },
-          {
-            type: 'plugin',
-            id: 'appmanager',
-            label: 'App manager'
-          },
-          {
-            type: 'plugin',
-            id: 'feedback',
-            label: 'Feedback'
-          },
-          {
-            type: 'plugin',
-            id: 'jobs',
-            label: 'Job overview'
-          },
-          {
-            type: 'plugin',
-            id: 'questionnaires',
-            label: 'Questionnaires'
-          },
-          {
-            type: 'plugin',
-            id: 'scripts',
-            label: 'Scripts'
-          }
-        ]
-      },
-      {
-        type: 'menu',
-        id: 'admin',
-        label: 'Admin',
-        items: [
-          {
-            type: 'plugin',
-            id: 'logmanager',
-            label: 'Log manager'
-          },
-          {
-            type: 'plugin',
-            id: 'menumanager',
-            label: 'Menu Manager'
-          },
-          {
-            type: 'plugin',
-            id: 'permissionmanager',
-            label: 'Permission Manager'
-          },
-          {
-            type: 'plugin',
-            id: 'scheduledjobs',
-            label: 'Scheduled Jobs'
-          },
-          {
-            type: 'plugin',
-            id: 'settings',
-            label: 'Settings'
-          },
-          {
-            type: 'plugin',
-            id: 'thememanager',
-            label: 'Theme Manager'
-          },
-          {
-            type: 'plugin',
-            id: 'usermanager',
-            label: 'User Manager'
-          },
-          {
-            type: 'plugin',
-            id: 'security-ui',
-            label: 'Security Manager'
-          }
-        ]
-      },
-      {
-        type: 'plugin',
-        id: 'useraccount',
-        label: 'Account'
       }
     ]
   },
-  loginHref: '/login',
-  helpLink: '{label: \'Help\', href: \'https://molgenis.gitbooks.io/molgenis/content/\'}',
-  authenticated: false,
-  showCookieWall: false,
-  version: '8.2.0-SNAPSHOT-PR-8570-28',
-  buildDate: '2019-08-20 13:57 UTC'
+  'loginHref': '/login',
+  'helpLink': {
+    'label': 'Help',
+    'href': 'https://molgenis.gitbook.io/molgenis/'
+  },
+  'authenticated': false,
+  'email': 'molgenis+anonymous@gmail.com',
+  'username': 'anonymous',
+  'roles': [
+    'ROLE_ANONYMOUS'
+  ],
+  'showCookieWall': false,
+  'version': '8.4.0-SNAPSHOT',
+  'buildDate': '2020-04-08 08:21 UTC',
+  'selectedTheme': '/css/bootstrap-4/bootstrap-molgenis-blue.min.css?1tzNyw'
 }
