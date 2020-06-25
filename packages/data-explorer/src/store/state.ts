c03bea98e521267495e2b3ef9f4a9b4fe990650f
Jelmer Veen <j.p.veen@rug.nl>
2020-06-25 15:13:36 +0200

    fix: reverted pre-emptive systable merge

--- packages/data-explorer/src/store/state.ts
+++ packages/data-explorer/src/store/state.ts
@@ -2,7 +2,7 @@ import ApplicationState from '@/types/ApplicationState'
 
 const state: ApplicationState = {
   toast: null,
-  settingsTable: 'sys_entity_DataExplorerEntitySettings',
+  settingsTable: 'de_dataexplorer_table_settings',
   tableName: null,
   tableData: null,
   tableMeta: null,
