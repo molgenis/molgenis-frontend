3e84e3063fca5b668a9da3ae624623abd463c77f
Jelmer Veen <j.p.veen@rug.nl>
2020-07-13 10:12:06 +0200

    fix: shaping settings library

--- packages/data-explorer/src/lib/settings.ts
+++ packages/data-explorer/src/lib/settings.ts
@@ -1,26 +1,18 @@
 import Axios from 'axios'
 import store from '../store/store'
 
-function getSettingsUrl () {
-  console.log('settingsurl')
-  return `${window.location.href.replace(window.location.hash, '').replace('index.html', '')}config/settings.xlsx`
-}
+const emx = 'settings.xlsx'
+const action = 'ADD_UPDATE_EXISTING'
+const currentAppLocationUrl = `${window.location.href.replace(window.location.hash, '').replace('/index.html', '')}`
+const settingsLocationUrl = `${currentAppLocationUrl}/config/${emx}`
+const molgenisSettingsEndpoint = `/plugin/importwizard/importByUrl?url=${settingsLocationUrl}&action=${action}`
 
-function setSettingsFormData (fileContents: string) {
-  let formData = new FormData()
-  let dataAsBlob = new Blob([fileContents], { type: 'application/xml' })
-  formData.append('file', dataAsBlob, 'settings.xlsx')
-  formData.append('action', 'ADD_UPDATE_EXISTING')
-  formData.append('notify', 'false')
-  return formData
-}
-
-function settingsByUrlFormdata () {
-  console.log('settings form data')
-  return `/plugin/importwizard/importByUrl?url=${getSettingsUrl()}`
-}
+// https://preview-frontend-pr-325-6.dev.molgenis.org/menu/main/dataexplorer?entity=plugin_data_data_explorer_entity_settings&hideselect=true
 
 const BootstrapExplorer = async () => {
+  let beforeSettings = Axios.get('/api/metadata/plugin_data_data_explorer_entity_settings').then(t =>
+    console.log('check before', t)
+  )
   // console.log('bootstrapping application')
   // let versionNumberRequest = await Axios.get('/config/version.txt') // this does not work on the server
   // const version = versionNumberRequest.data
@@ -30,17 +22,15 @@ const BootstrapExplorer = async () => {
   // const fileRequest = await Axios.get('/config/settings.xlsx')
   // const settingsFormData = setSettingsFormData(fileRequest.data)
   // console.log(fileRequest)
-  const t = settingsByUrlFormdata()
-  console.log(t)
-  let x = await Axios.post(t).catch(e => console.log(e))
-  console.log('axios', x)
+  let x = await Axios.post(molgenisSettingsEndpoint).catch(e => console.log(e))
+  console.log('Settings post:', x)
 
   // /plugin/importwizard/importByUrl?notify=false&entityTypeId=demo&url=http://localhost:8080/config/settings.xlsx
   if (store.getters.hasEditRights) { }
 
   // Axios.get('/config/settingstable.xlsx').then(t => console.log(t))
-  let isSettingsTableAvailable = Axios.get('/api/metadata/root_hospital_patients?flattenAttributes=true').then(t =>
-    console.log(t)
+  let isSettingsTableAvailable = Axios.get('/api/metadata/plugin_data_data_explorer_entity_settings').then(t =>
+    console.log('check after', t)
   )
 }
 
