71514bb203d69d0e9baf703c17e53286cc1b65f8
connoratrug <c.stroomberg@rug.nl>
2020-08-08 16:21:45 +0200

    task(data-row-edit, app-manager): fix style issues

--- packages/app-manager/src/main.ts
+++ packages/app-manager/src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import AppManager from './AppManager.vue'
 import store from './store'
 
+import 'font-awesome/css/font-awesome.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
