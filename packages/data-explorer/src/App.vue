870f5244dc1a8e91bf130f9f8cea07be04cd90a6
Jelmer Veen <j.p.veen@rug.nl>
2020-09-03 12:04:38 +0200

    feat(data-explorer): lerna single build test-case

--- packages/data-explorer/src/App.vue
+++ packages/data-explorer/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <page-component id="app" @contextLoaded="setContext">
     <router-view />
+    <h2>Hello changed package and preview got built =)</h2>
   </page-component>
 </template>
 
