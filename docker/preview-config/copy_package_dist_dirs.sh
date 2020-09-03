99a17cc3b8a08530850445e827aa38fcaea3ca34
Jelmer Veen <j.p.veen@rug.nl>
2020-09-03 13:56:09 +0200

    fix(preview): removed experimental added curl to unchanged packages

--- docker/preview-config/copy_package_dist_dirs.sh
+++ docker/preview-config/copy_package_dist_dirs.sh
@@ -4,10 +4,12 @@ rm -rf docker/preview-config/dist
 mkdir -p docker/preview-config/dist/@molgenis-ui
 mkdir -p docker/preview-config/dist/@molgenis-experimental
 for file in packages/*; do 
-    if test -f "${file}/dist"; then
     mkdir -p "docker/preview-config/dist/@molgenis-ui/$(basename "$file")/dist"
-    mkdir -p "docker/preview-config/dist/@molgenis-experimental/$(basename "$file")/dist"
+    # mkdir -p "docker/preview-config/dist/@molgenis-experimental/$(basename "$file")/dist"
+    if test -f "${file}/dist"; then
     cp -rf "${file}/dist" "docker/preview-config/dist/@molgenis-ui/$(basename "$file")"
-    cp -rf "${file}/dist" "docker/preview-config/dist/@molgenis-experimental/$(basename "$file")"
+    # cp -rf "${file}/dist" "docker/preview-config/dist/@molgenis-experimental/$(basename "$file")"
+    else
+    curl -o "https://unpkg.com/browse/@molgenis-ui/${file}/dist/" --output-dir "docker/preview-config/dist/@molgenis-ui/$(basename "$file")"     
     fi
 done
