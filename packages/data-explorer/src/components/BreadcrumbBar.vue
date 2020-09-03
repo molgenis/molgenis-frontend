4ce791233673cba0db835e8b56f62cb83dfe2747
connoratrug <c.stroomberg@rug.nl>
2020-09-03 14:21:43 +0200

    remove chevron from breadcrumb bar, in favor of just having the dropdown

--- packages/data-explorer/src/components/BreadcrumbBar.vue
+++ packages/data-explorer/src/components/BreadcrumbBar.vue
@@ -1,15 +1,11 @@
 <template>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li v-for="crumb in tail" :key="crumb.label" class="breadcrumb-item dropdown">
-        <a v-if="crumb.link" :href="crumb.link">
-          {{crumb.label}}
-          <font-awesome-icon
-            :icon="['fas', 'chevron-down']" size="xs"
-            @mouseover="showDropdown($event, crumb)"
-            @mouseleave="hideDropdown($event, crumb)">
-          </font-awesome-icon>
-        </a>
+      <li v-for="crumb in tail" :key="crumb.label" class="breadcrumb-item dropdown"
+        @mouseover="showDropdown($event, crumb)"
+        @mouseleave="hideDropdown($event, crumb)"
+      >
+        <a v-if="crumb.link" :href="crumb.link">{{crumb.label}}</a>
         <template v-else>{{crumb.label}}</template>
         <dropdown-list
           v-show="crumb.showDropdown"
@@ -25,16 +21,11 @@
 
 <script>
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import DropdownList from '@/components/DropdownList'
-
-library.add(faChevronDown)
 
 export default {
   name: 'BreadcrumbBar',
-  components: { DropdownList, FontAwesomeIcon },
+  components: { DropdownList },
   props: {
     breadcrumbs: {
       type: Array,
