e767e8e47f038bfc1d5172827cbbfcc83b19593c
Jelmer Veen <j.p.veen@rug.nl>
2020-09-03 14:34:13 +0200

    fix output dir

--- docker/multi-build/get_dists.sh
+++ docker/multi-build/get_dists.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Creates all dists using unpackage, so that we can get a preview without building everything
 for file in packages/*; do 
-    mkdir -p "${file}/dist"  
-    curl -o "https://unpkg.com/browse/@molgenis-ui/${file}/dist/" --output-dir "${file}/dist"   
+    mkdir -p "${WORKSPACE}/packages/${file}/dist"  
+    curl -O "https://unpkg.com/browse/@molgenis-ui/${file}/dist/" --output "${WORKSPACE}/packages/${file}/dist/"    
 done
