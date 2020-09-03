9426cc5ab24c601d1d6406a02de802f4c0fab92c
connoratrug <c.stroomberg@rug.nl>
2020-09-03 12:30:55 +0200

    add chevron to indicate downdown and fix tablet usage

--- packages/data-explorer/src/components/BreadcrumbBar.vue
+++ packages/data-explorer/src/components/BreadcrumbBar.vue
@@ -1,11 +1,15 @@
 <template>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li v-for="crumb in tail" :key="crumb.label" class="breadcrumb-item dropdown"
-        @mouseover="showDropdown($event, crumb)"
-        @mouseleave="hideDropdown($event, crumb)"
-      >
-        <a v-if="crumb.link" :href="crumb.link">{{crumb.label}}</a>
+      <li v-for="crumb in tail" :key="crumb.label" class="breadcrumb-item dropdown">
+        <a v-if="crumb.link" :href="crumb.link">
+          {{crumb.label}}
+          <font-awesome-icon
+            :icon="['fas', 'chevron-down']" size="xs"
+            @mouseover="showDropdown($event, crumb)"
+            @mouseleave="hideDropdown($event, crumb)">
+          </font-awesome-icon>
+        </a>
         <template v-else>{{crumb.label}}</template>
         <dropdown-list
           v-show="crumb.showDropdown"
@@ -21,11 +25,16 @@
 
 <script>
 import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import DropdownList from '@/components/DropdownList'
+
+library.add(faChevronDown)
 
 export default {
   name: 'BreadcrumbBar',
-  components: { DropdownList },
+  components: { DropdownList, FontAwesomeIcon },
   props: {
     breadcrumbs: {
       type: Array,
--- packages/data-explorer/tests/unit/components/DropdownList.spec.ts
+++ packages/data-explorer/tests/unit/components/DropdownList.spec.ts
@@ -15,7 +15,6 @@ describe('DropdownList', () => {
   }
 
   beforeEach(() => {
-  
     wrapper = shallowMount(DropdownList, { propsData, stubs })
   })
 
