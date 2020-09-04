35d255577dbc460e613531a7410f07f3b27ce407
connoratrug <c.stroomberg@rug.nl>
2020-09-04 09:56:07 +0200

    fix example unit test

--- packages/molgenis-components/tests/unit/example.spec.ts
+++ packages/molgenis-components/tests/unit/example.spec.ts
@@ -1,12 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppButton from '@/components/AppButton.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('AppButton.vue', () => {
+  it('renders the button with the passed slot', () => {
+    const wrapper = shallowMount(AppButton, {
+      slots: {
+        default: 'Hy there'
+      }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch('Hy there')
   })
 })
