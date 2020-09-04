4f3daf5c63eda78d35809bd86885c63bad8e5006
connoratrug <c.stroomberg@rug.nl>
2020-09-04 16:58:51 +0200

    extend test cov

--- packages/molgenis-components/tests/unit/example.spec.ts
+++ packages/molgenis-components/tests/unit/example.spec.ts
@@ -2,12 +2,19 @@ import { shallowMount } from '@vue/test-utils'
 import AppButton from '@/components/AppButton.vue'
 
 describe('AppButton.vue', () => {
+  const slots = {
+    default: 'Hy there'
+  }
   it('renders the button with the passed slot', () => {
-    const wrapper = shallowMount(AppButton, {
-      slots: {
-        default: 'Hy there'
-      }
-    })
+    const wrapper = shallowMount(AppButton, { slots })
     expect(wrapper.text()).toMatch('Hy there')
+  })
+  it('fire "click" and "gator" event when clicked', async (done) => {
+    const wrapper = shallowMount(AppButton, { slots })
+    wrapper.find('button').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.emitted().click).toBeTruthy()
+    expect(wrapper.emitted().gator).toBeTruthy()
+    done()
   })
 })
