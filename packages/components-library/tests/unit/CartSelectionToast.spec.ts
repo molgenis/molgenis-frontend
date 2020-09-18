c44159666b339c76ed29680f52c3bb55bf177e46
Max Postema <m.e.postema@rug.nl>
2020-09-18 15:47:42 +0200

    choir: fix linter

--- packages/components-library/tests/unit/CartSelectionToast.spec.ts
+++ packages/components-library/tests/unit/CartSelectionToast.spec.ts
@@ -5,15 +5,14 @@ import { BootstrapVue } from 'bootstrap-vue'
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
-const propsData = { clickHandler: ()=>{}, cartSelectionText: 'test' }
+const propsData = { clickHandler: () => {}, cartSelectionText: 'test' }
 describe('CartSelectionToast.vue', () => {
   it('renders the CartSelectionToast', () => {
-    const wrapper = shallowMount(CartSelectionToast, { localVue , propsData })
+    const wrapper = shallowMount(CartSelectionToast, { localVue, propsData })
     expect(wrapper.text()).toMatch('Request items')
   })
   it('Can remove items from the given (value/v-model) list', async () => {
-
-    const wrapper = shallowMount(CartSelectionToast, { localVue, propsData: { ...propsData, value: ['1', '2', '3'] }})
+    const wrapper = shallowMount(CartSelectionToast, { localVue, propsData: { ...propsData, value: ['1', '2', '3'] } })
     // @ts-ignore
     wrapper.vm.removeItem('2')
     await wrapper.vm.$nextTick()
@@ -21,15 +20,15 @@ describe('CartSelectionToast.vue', () => {
     expect(wrapper.emitted().input[0][0]).toEqual(['1', '3'])
   })
   it('Can displays show/hide according to previewToggle status', () => {
-    const wrapper = shallowMount(CartSelectionToast, { localVue , propsData })
+    const wrapper = shallowMount(CartSelectionToast, { localVue, propsData })
     // @ts-ignore
     expect(wrapper.vm.selectionText).toMatch('Show') // Default value
     // @ts-ignore
-    wrapper.vm.previewToggle = true;
+    wrapper.vm.previewToggle = true
     // @ts-ignore
     expect(wrapper.vm.selectionText).toMatch('Hide')
     // @ts-ignore
-    wrapper.vm.previewToggle = false;
+    wrapper.vm.previewToggle = false
     // @ts-ignore
     expect(wrapper.vm.selectionText).toMatch('Show')
   })
