f30fba3caa7a32e2b89ea7c5b0dec1016c031b46
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-09-24 15:51:05 +0200

    feat(components-library) :checkbox filter (#383)
    feat(components-library): Checkbox filter
    
    Allows creating filter from multiple lazy loaded filter options

--- packages/components-library/tests/unit/ui/CartSelectionToast.spec.ts
+++ packages/components-library/tests/unit/ui/CartSelectionToast.spec.ts
@@ -1,5 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import CartSelectionToast from '@/components/CartSelectionToast.vue'
+import CartSelectionToast from '@/components/ui/CartSelectionToast.vue'
 
 import { BootstrapVue } from 'bootstrap-vue'
 const localVue = createLocalVue()
