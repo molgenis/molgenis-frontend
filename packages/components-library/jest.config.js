9459d17ce5cbded297bf6f8305c4c28be3712fe9
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-09-25 16:56:53 +0200

    feat(components-library): Add Number filter component (#386)
    feat(components-library): Add Number filter component
    
    - Move over number filter from filters repo

--- packages/components-library/jest.config.js
+++ packages/components-library/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
+  },
   collectCoverage: true
 }
--- packages/components-library/src/main.ts
+++ packages/components-library/src/main.ts
@@ -1,9 +1,10 @@
 import CartSelectionToast from './components/ui/CartSelectionToast.vue'
 import CheckboxFilter from './components/filters/CheckboxFilter.vue'
 import StringFilter from './components/filters/StringFilter.vue'
+import NumberFilter from './components/filters/NumberFilter.vue'
 
-export { CartSelectionToast, CheckboxFilter, StringFilter }
+export { CartSelectionToast, CheckboxFilter, StringFilter, NumberFilter }
 
 export default {
-  CartSelectionToast, CheckboxFilter, StringFilter
+  CartSelectionToast, CheckboxFilter, StringFilter, NumberFilter
 }
