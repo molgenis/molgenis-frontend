cfcc51d56e39d2461c69f36f10abd34c632b572c
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-10-07 09:40:18 +0200

    feat(components-library): Add active filters component (#393)
    feat(components-library): Add active filters component
    
    - Move over ActiveFilters component form molgenis/filter-ui repository

--- packages/components-library/src/main.ts
+++ packages/components-library/src/main.ts
@@ -2,10 +2,26 @@ import CartSelectionToast from './components/ui/CartSelectionToast.vue'
 import CheckboxFilter from './components/filters/CheckboxFilter.vue'
 import StringFilter from './components/filters/StringFilter.vue'
 import NumberFilter from './components/filters/NumberFilter.vue'
+import DateTimeFilter from './components/filters/DateTimeFilter.vue'
+import ActiveFilters from './components/filters/ActiveFilters.vue'
 import FilterCard from './components/filters/containers/FilterCard.vue'
 
-export { CartSelectionToast, CheckboxFilter, StringFilter, NumberFilter, FilterCard }
+export {
+  ActiveFilters,
+  CartSelectionToast,
+  CheckboxFilter,
+  DateTimeFilter,
+  FilterCard,
+  NumberFilter,
+  StringFilter
+}
 
 export default {
-  CartSelectionToast, CheckboxFilter, StringFilter, NumberFilter, FilterCard
+  ActiveFilters,
+  CartSelectionToast,
+  CheckboxFilter,
+  DateTimeFilter,
+  FilterCard,
+  NumberFilter,
+  StringFilter
 }
