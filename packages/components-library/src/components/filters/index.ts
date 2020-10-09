a54aed7fb1649afecc9c68f1da1d8baf0834b56c
connoratrug <c.stroomberg@rug.nl>
2020-10-09 14:25:04 +0200

    remove unused code

--- packages/components-library/src/components/filters/index.ts
+++ packages/components-library/src/components/filters/index.ts
@@ -1,7 +1,3 @@
-import BootstrapVue from 'bootstrap-vue'
-import Vue from 'vue'
-Vue.use(BootstrapVue)
-
 export { default as CheckboxFilter } from './CheckboxFilter.vue'
 export { default as StringFilter } from './StringFilter.vue'
 export { default as NumberFilter } from './NumberFilter.vue'
