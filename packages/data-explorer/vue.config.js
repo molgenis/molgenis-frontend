3a66434dc763676f5322f9fe71fff479764f68dd
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-10-26 14:12:45 +0100

    chore(data-explorer): fix e2e test build by not following symlinks  (#422)
    Stop symlinking in data-explorer build, work around for lerna buil fail due to symlink issues

--- packages/data-explorer/vue.config.js
+++ packages/data-explorer/vue.config.js
@@ -27,6 +27,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? pkgName + '/dist/'
     : '/',
+  chainWebpack: (config) => {
+    // stop lerna from sym linking to empty components-library folder
+    config.resolve.symlinks(false)
+  },
   configureWebpack: config => {
     config.plugins.push(
       new BannerPlugin({
