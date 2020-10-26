e2f260c71c9700c35ee922f47777d91436e39e68
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-10-26 10:07:21 +0100

    chore(data-explorer): bump components lib (#420)
    chore(data-explorer): bump components lib
    
    Update data-explorer for use with 1.x version of components lib
    - Import dependacies needed for componets into the data-explorer
    - Update the import of the lib components in the views (Data and Filter View)
    - Add workaround for vue slider ts issue to tsconfig
    - skip ie11 e2e for data-explorer

--- packages/data-explorer/src/main.ts
+++ packages/data-explorer/src/main.ts
@@ -3,10 +3,56 @@ import 'bootstrap'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import { BootstrapVue } from 'bootstrap-vue'
 
 // @ts-ignore
 import i18n from '@molgenis/molgenis-i18n-js'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// @ts-ignore
+import DateRangePicker from 'vue2-daterange-picker'
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+// @ts-ignore
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+// @ts-ignore
+import draggable from 'vuedraggable'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faCaretRight,
+  faChevronRight,
+  faChevronLeft,
+  faChevronUp,
+  faCog,
+  faEdit,
+  faExclamationTriangle,
+  faMinus,
+  faPlay,
+  faPlus,
+  faPlusSquare,
+  faShoppingBag,
+  faShoppingCart,
+  faSlidersH,
+  faSpinner,
+  faSearch,
+  faStore,
+  faTh,
+  faThList,
+  faTimes,
+  faTrash,
+  faUpload
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCaretRight, faChevronLeft, faChevronRight, faChevronUp, faCog, faEdit, faExclamationTriangle,
+  faMinus, faPlay, faPlus, faPlusSquare, faShoppingBag, faSlidersH, faShoppingCart, faSpinner,
+  faSearch, faStore, faTh, faThList, faTimes, faTrash, faUpload
+)
+
+Vue.use(BootstrapVue)
+Vue.component('draggable', draggable)
+Vue.component('DateRangePicker', DateRangePicker)
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
+Vue.component('VueSlider', VueSlider)
 
 Vue.config.productionTip = false
 
