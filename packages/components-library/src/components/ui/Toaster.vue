67359113a7c3dd320808544e90c15db1164fe3e0
Jeroen van Veen <jvanveen@protonmail.com>
2020-11-19 16:47:02 +0100

    feat: add Toaster component

---
+++ packages/components-library/src/components/ui/Toaster.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="c-toaster" :class="{[position]: true}">
+    <div v-for="(toast, index) in toasts" :key="index"
+      class="mg-toast alert alert-success alert-dismissible fade show"
+      :class="{[`alert-${toast.type}`]: true}"
+    >
+      <div class="mg-toast-message" v-html="toast.message"></div>
+
+      <button type="button" class="close" @click="clearToast(toast)">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+const defaultTimeout = 3000
+
+export default {
+  data: function () {
+    return {
+      toasts: [...this.value]
+    }
+  },
+  name: 'Toaster',
+  methods: {
+    clearToast (toast) {
+      this.toasts.splice(this.toasts.indexOf(toast), 1)
+      /**
+      * v-model return value
+      * @event input
+      * @property {Object[]} Toasts array with toast removed
+      */
+      this.$emit('input', [...this.toasts])
+    }
+  },
+  props: {
+    /**
+     * List of toasts to show (see types/Toast.ts)
+     * @model
+     */
+    value: {
+      type: Array,
+      required: true
+    },
+    /**
+     * Position of toast in window
+     * Fixed positioning: 'top-left', 'bottom-left', 'top-right', 'bottom-right'
+     * Or Inline using: 'inline'
+     */
+    position: {
+      type: String,
+      default: () => 'bottom-right'
+    }
+  },
+  watch: {
+    value: {
+      handler: function (newValue, oldValue) {
+        this.$set(this, 'toasts', [...newValue])
+
+        // Only process timeout for new toasts. Old value is empty
+        // when called on creation, due to immediately watch property.
+        if (oldValue && newValue.length < oldValue.length) return
+
+        for (const toast of newValue) {
+          // Timeout already scheduled.
+          if (toast.timeout === -1) continue
+          // New toast has an explicitly disabled timeout.
+          if (toast.timeout === 0) continue
+
+          if (!toast.timeout) {
+            toast.timeout = defaultTimeout
+          }
+
+          setTimeout(() => {
+            this.clearToast(toast)
+          }, toast.timeout)
+
+          toast.timeout = -1
+        }
+      },
+      immediate: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$spacer: 1rem;
+
+.c-toaster {
+    &.top-left,
+    &.top-right {
+      position: fixed;
+      top: $spacer;
+    }
+
+    &.bottom-left,
+    &.bottom-right {
+      position: fixed;
+      bottom: $spacer;
+    }
+
+    &.bottom-left,
+    &.top-left {
+      left: $spacer;
+    }
+
+    &.bottom-right,
+    &.top-right {
+      right: $spacer;
+    }
+
+    &:not(.inline) {
+        width: 20rem;
+        z-index: 1060;  // Bootstrap popover z-index
+
+        @media (max-width: 576px) {
+          width: 100%;
+          padding: 0 $spacer;
+
+          &.bottom-left,
+          &.top-left {
+            left: 0;
+          }
+
+          &.bottom-right,
+          &.top-right {
+            right: 0;
+          }
+
+        }
+    }
+}
+</style>
+
+<docs>
+  This is a notification component.
+
+  ## Example
+
+  ```jsx
+  let model = []
+  const addToast = (message, type, timeout) => model.push({message, type, timeout})
+  const mockVuex = (e) => model = e
+
+  <Toaster v-model="model" v-on:input="mockVuex"></Toaster>
+  <button class="btn btn-primary" v-on:click="addToast('new message', 'success', 0)">Add toast</button>
+  <button class="btn btn-primary" v-on:click="addToast('timeout message', 'danger', 1500)">Add toast (timeout)</button>
+  <div>{{model}}</div>
+  ```
+</docs>
