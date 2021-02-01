184527dcd0c52f35f8753f049c265be8685b2b61
connoratrug <47183404+connoratrug@users.noreply.github.com>
2021-02-01 12:16:14 +0100

    fix(components-library): emit updated model on size change (#514)
    Update the model when the size changes
    Emit a input event passing the updated model to notify listeners

--- packages/components-library/src/components/ui/Pagination.vue
+++ packages/components-library/src/components/ui/Pagination.vue
@@ -44,7 +44,7 @@
 
     <div class="form-check form-check-inline">
       <label class="form-check-label mr-2" for="page-size">{{i18n['per page']}}</label>
-      <select class="form-control" id="page-size" v-model="localValue.size">
+      <select class="form-control" id="page-size" v-model="localValue.size" @change="handleSizeChange">
         <option v-for="pageSize of pageSizes" :value="pageSize" :key="pageSize">{{pageSize}}</option>
       </select>
     </div>
@@ -55,68 +55,6 @@
 
 <script>
 export default {
-  async created () {
-    this.localValue = { ...this.localValue, ...this.value }
-  },
-  computed: {
-    navigationText () {
-      if (this.pageCount <= 1) {
-        return `${this.i18n.items}: ${this.localValue.count}`
-      } else {
-        return `${this.i18n.page} ${this.localValue.page}/${this.pageCount} (${this.localValue.count} ${this.i18n.items})`
-      }
-    },
-    pageCount () {
-      if (!this.localValue.count) { return 0 }
-      return Math.ceil(this.localValue.count / this.localValue.size)
-    },
-    pageNumbers () {
-      const pages = []
-      for (let i = this.pageRange.left; i <= this.pageRange.right; i++) { pages.push(i) }
-      return pages
-    },
-    /**
-     * Calculations that determine which pages must be rendered.
-     * See the computed `pageNumbers` method for its usage.
-     */
-    pageRange () {
-      const edge = Math.floor(this.visiblePages / 2)
-      const start = this.localValue.page <= edge
-      const end = this.localValue.page >= (Math.floor(this.localValue.count / this.localValue.size) - edge)
-
-      let left, right
-
-      if (start) {
-        left = 1
-        right = (this.pageCount > this.visiblePages) ? this.visiblePages : this.pageCount
-      } else if (end) {
-        left = (this.pageCount - this.visiblePages) > 0 ? (this.pageCount - this.visiblePages + 1) : 1
-        right = this.pageCount
-      } else {
-        // Must be within the mid-range.
-        left = this.localValue.page - edge
-        right = this.localValue.page + edge
-      }
-
-      return { left, right }
-    }
-  },
-  data: function () {
-    return {
-      localValue: {
-        size: 20,
-        page: 1,
-        loading: false,
-        count: 0
-      }
-    }
-  },
-  methods: {
-    updateValue (value) {
-      this.localValue = { ...this.localValue, ...this.value, ...value }
-      this.$emit('input', { ...this.localValue })
-    }
-  },
   name: 'Pagination',
   props: {
     /**
@@ -165,22 +103,81 @@ export default {
       }
     }
   },
+  data: function () {
+    return {
+      localValue: {
+        size: 20,
+        page: 1,
+        loading: false,
+        count: 0
+      }
+    }
+  },
+  computed: {
+    navigationText () {
+      if (this.pageCount <= 1) {
+        return `${this.i18n.items}: ${this.localValue.count}`
+      } else {
+        return `${this.i18n.page} ${this.localValue.page}/${this.pageCount} (${this.localValue.count} ${this.i18n.items})`
+      }
+    },
+    pageCount () {
+      if (!this.localValue.count) { return 0 }
+      return Math.ceil(this.localValue.count / this.localValue.size)
+    },
+    pageNumbers () {
+      const pages = []
+      for (let i = this.pageRange.left; i <= this.pageRange.right; i++) { pages.push(i) }
+      return pages
+    },
+    /**
+     * Calculations that determine which pages must be rendered.
+     * See the computed `pageNumbers` method for its usage.
+     */
+    pageRange () {
+      const edge = Math.floor(this.visiblePages / 2)
+      const start = this.localValue.page <= edge
+      const end = this.localValue.page >= (Math.floor(this.localValue.count / this.localValue.size) - edge)
+
+      let left, right
+
+      if (start) {
+        left = 1
+        right = (this.pageCount > this.visiblePages) ? this.visiblePages : this.pageCount
+      } else if (end) {
+        left = (this.pageCount - this.visiblePages) > 0 ? (this.pageCount - this.visiblePages + 1) : 1
+        right = this.pageCount
+      } else {
+        // Must be within the mid-range.
+        left = this.localValue.page - edge
+        right = this.localValue.page + edge
+      }
+
+      return { left, right }
+    }
+  },
+  methods: {
+    updateValue (value) {
+      this.localValue = { ...this.localValue, ...this.value, ...value }
+      this.$emit('input', { ...this.localValue })
+    },
+    handleSizeChange () {
+      this.updateValue({ page: 1, size: this.localValue.size })
+    }
+  },
   watch: {
     /**
      * Sync the local state when the external state changes.
      */
     value: {
-      handler (value) { this.localValue = value },
+      handler (value) {
+        this.localValue = value
+      },
       deep: true
-    },
-    /**
-     * A separate listener is needed here, because its
-     * not feasible to detect size property changes from
-     * the deep value watcher (See https://vuejs.org/v2/api/#vm-watch)
-     */
-    'value.size' () {
-      this.updateValue({ page: 1 })
     }
+  },
+  async created () {
+    this.localValue = { ...this.localValue, ...this.value }
   }
 }
 </script>
--- packages/components-library/tests/unit/ui/Pagination.spec.ts
+++ packages/components-library/tests/unit/ui/Pagination.spec.ts
@@ -28,30 +28,46 @@ const buildWrapper = async (propsData: any, attrs: any = null) => {
 }
 
 describe('Pagination.vue', () => {
-  describe('Regardless of using a router', () => {
-    it('navigates within a paginated range', async () => {
-      propsData = {
-        value: {
-          count: 100, loading: false, size: 20, page: 4
-        },
-        visiblePages: 3
-      }
-      const wrapper = await buildWrapper(propsData)
-      // The model is leading to determine the current page:
-      expect(wrapper.vm.localValue).toEqual({ count: 100, loading: false, page: 4, size: 20 })
-      await wrapper.find('.t-page-next').trigger('click')
-      expect(propsData.value.page).toEqual(5)
-      // // Next page is disabled when on the last page:
-      expect(wrapper.find('.t-page-next[disabled]').exists()).toBe(true)
+  it('navigates within a paginated range', async () => {
+    propsData = {
+      value: {
+        count: 100, loading: false, size: 20, page: 4
+      },
+      visiblePages: 3
+    }
+    const wrapper = await buildWrapper(propsData)
+    // The model is leading to determine the current page:
+    expect(wrapper.vm.localValue).toEqual({ count: 100, loading: false, page: 4, size: 20 })
+    await wrapper.find('.t-page-next').trigger('click')
+    expect(propsData.value.page).toEqual(5)
+    // // Next page is disabled when on the last page:
+    expect(wrapper.find('.t-page-next[disabled]').exists()).toBe(true)
 
-      await wrapper.find('.t-page-prev').trigger('click')
-      expect(propsData.value.page).toEqual(4)
+    await wrapper.find('.t-page-prev').trigger('click')
+    expect(propsData.value.page).toEqual(4)
 
-      // go back to page 1
-      await wrapper.find('.t-page-prev').trigger('click')
-      await wrapper.find('.t-page-prev').trigger('click')
-      await wrapper.find('.t-page-prev').trigger('click')
-      expect(propsData.value.page).toEqual(1)
-    })
+    // go back to page 1
+    await wrapper.find('.t-page-prev').trigger('click')
+    await wrapper.find('.t-page-prev').trigger('click')
+    await wrapper.find('.t-page-prev').trigger('click')
+    expect(propsData.value.page).toEqual(1)
+  })
+
+  it('it emits the updated model when the size is changed', async () => {
+    propsData = {
+      value: { count: 100, loading: false, size: 20, page: 4 },
+      visiblePages: 3
+    }
+    const wrapper = await buildWrapper(propsData)
+    const options = await wrapper.find('select').findAll('option')
+
+    await options.at(0).setSelected()
+    expect(wrapper.emitted().input[0][0]).toEqual({ count: 100, loading: false, page: 1, size: 10 })
+    await options.at(1).setSelected()
+    expect(wrapper.emitted().input[1][0]).toEqual({ count: 100, loading: false, page: 1, size: 20 })
+    await options.at(2).setSelected()
+    expect(wrapper.emitted().input[2][0]).toEqual({ count: 100, loading: false, page: 1, size: 50 })
+    await options.at(0).setSelected()
+    expect(wrapper.emitted().input[3][0]).toEqual({ count: 100, loading: false, page: 1, size: 10 })
   })
 })
