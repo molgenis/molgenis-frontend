dd5fe0ff46a4f3a2fd73ab9d6522fbfed31e02e4
Jeroen van Veen <jvanveen@protonmail.com>
2021-03-05 16:49:22 +0100

    fix: review feedback

--- packages/data-explorer/src/types/RouteQuery.ts
+++ packages/data-explorer/src/types/RouteQuery.ts
@@ -5,5 +5,5 @@ export type RouteQuery = {
     filter?: string // hashed filter object
     hideSidebar?: 'false' | 'true'
     hide?: string // comma separated list of column names
-    view?: string
+    view?: 'CardView' | 'TableView'
 }
