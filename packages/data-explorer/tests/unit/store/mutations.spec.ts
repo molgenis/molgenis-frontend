bcdc07dd0b02798ac6d6209b158bb3ec3d384f82
connoratrug <c.stroomberg@rug.nl>
2021-03-12 15:23:31 +0100

    add custom detail prop to state

--- packages/data-explorer/tests/unit/store/mutations.spec.ts
+++ packages/data-explorer/tests/unit/store/mutations.spec.ts
@@ -173,6 +173,7 @@ describe('mutations', () => {
         settingsRowId: null,
         collapseLimit: 5,
         customCardCode: null,
+        customDetailCode: null,
         customCardAttrs: '',
         isShop: false,
         defaultFilters: []
