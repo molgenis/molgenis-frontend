c9bd77163b10ec208e89279a4345798a8477fd53
Jelmer Veen <jelmerveen@pennions.com>
2021-03-19 15:39:39 +0100

    Linting/gobal to local linting (#577)
    fix: moved eslint to component-lib and data-explorer
    
    Co-authored-by: Jelmer Veen <j.p.veen@rug.nl>

---
+++ packages/data-explorer/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaFeatures": {
+            "modules": true
+        },
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "root": true,
+    "globals": {
+        "globalThis": true
+    },
+    "ignorePatterns": [],
+    "overrides": [
+        {
+            "files": ["**/*.ts"],
+            "parser": "@typescript-eslint/parser",
+            "extends": [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended"
+            ],
+            "rules": {
+                "no-case-declarations": "off",
+                "@typescript-eslint/no-empty-function": "off",
+                "@typescript-eslint/no-var-requires": "off",
+                "@typescript-eslint/explicit-module-boundary-types": "off",
+                "@typescript-eslint/no-explicit-any": "off",
+                "@typescript-eslint/ban-types": "off",
+                "@typescript-eslint/ban-ts-comment": "off"
+            }
+        },
+        {
+            "parser": "vue-eslint-parser",
+            "files": ["**/*.vue"],
+            "extends": [
+                "plugin:vue/recommended"
+            ],
+            "rules": {
+                "vue/no-v-html": "off",
+                "vue/require-default-prop": "off",
+                "vue/max-attributes-per-line": ["error", {
+                    "singleline": 3,
+                    "multiline": {
+                      "max": 2,
+                      "allowFirstLine": false
+                    }
+                }]
+            }
+        }
+    ]
+}
