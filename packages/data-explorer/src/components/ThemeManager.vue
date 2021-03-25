de279c54cefabd84a0637a595e002fa065dd9f28
Jeroen van Veen <jvanveen@protonmail.com>
2021-03-25 12:29:39 +0100

    fix: linting errors

--- packages/data-explorer/src/components/ThemeManager.vue
+++ packages/data-explorer/src/components/ThemeManager.vue
@@ -1,69 +1,98 @@
-
 <template>
   <div class="container my-4">
     <h2>Theme Manager</h2>
 
     <div class="custom-control custom-radio mt-3">
-        <input type="radio" class="custom-control-input" id="theme-selection-public" v-model="selectionMethod" name="radio-stacked" value="public">
-        <label class="custom-control-label" for="theme-selection-public">Select a public theme</label>
+      <input
+        id="theme-selection-public" v-model="selectionMethod"
+        type="radio"
+        class="custom-control-input" name="radio-stacked"
+        value="public"
+      >
+      <label class="custom-control-label" for="theme-selection-public">Select a public theme</label>
     </div>
 
     <div class="custom-control custom-radio">
-        <input type="radio" class="custom-control-input" id="theme-selection-custom" v-model="selectionMethod" name="radio-stacked" value="custom">
-        <label class="custom-control-label" for="theme-selection-custom">URL to a custom theme</label>
+      <input
+        id="theme-selection-custom" v-model="selectionMethod"
+        type="radio"
+        class="custom-control-input" name="radio-stacked"
+        value="custom"
+      >
+      <label class="custom-control-label" for="theme-selection-custom">URL to a custom theme</label>
     </div>
 
     <div class="custom-control custom-radio mb-4">
-        <input type="radio" class="custom-control-input" id="theme-selection-generated" v-model="selectionMethod" name="radio-stacked" value="generated">
-        <label class="custom-control-label" for="theme-selection-generated">Create your own theme</label>
+      <input
+        id="theme-selection-generated" v-model="selectionMethod"
+        type="radio"
+        class="custom-control-input" name="radio-stacked"
+        value="generated"
+      >
+      <label class="custom-control-label" for="theme-selection-generated">Create your own theme</label>
     </div>
 
     <div v-if="selectionMethod === 'public'">
-        <div class="form-group">
-            <label for="bootstrap-theme-select">Available Themes</label>
-            <select class="form-control" id="bootstrap-theme-select" v-model="selectedTheme">
-                <option v-for="theme in themes" :value="theme.id" :key="theme.id">{{theme.name || theme.id}}</option>
-            </select>
-        </div>
+      <div class="form-group">
+        <label for="bootstrap-theme-select">Available Themes</label>
+        <select id="bootstrap-theme-select" v-model="selectedTheme" class="form-control">
+          <option v-for="_theme in themes" :key="_theme.id" :value="_theme.id">
+            {{ _theme.name || _theme.id }}
+          </option>
+        </select>
+      </div>
     </div>
 
     <div v-else-if="selectionMethod === 'custom'">
-        <div class="form-group">
-            <label for="theme-url">Theme url</label>
-            <input type="text" class="form-control" id="theme-url" autocomplete="off" v-model="themeUrl">
-            <small id="emailHelp" class="form-text text-muted">Url to a customized <a href="https://github.com/molgenis/molgenis-theme">molgenis-theme</a> Bootstrap 4 file.</small>
-        </div>
-        <div class="form-group">
-            <label for="theme-url-legacy">Theme url (legacy)</label>
-            <input type="text" class="form-control" id="theme-url-legacy" autocomplete="off" v-model="themeUrlLegacy">
-            <small id="emailHelp" class="form-text text-muted">Url to a customized <a href="https://github.com/molgenis/molgenis-theme">molgenis-theme</a> Bootstrap 3 file.</small>
-        </div>
+      <div class="form-group">
+        <label for="theme-url">Theme url</label>
+        <input
+          id="theme-url" v-model="themeUrl"
+          type="text"
+          class="form-control" autocomplete="off"
+        >
+        <small id="emailHelp" class="form-text text-muted">Url to a customized <a href="https://github.com/molgenis/molgenis-theme">molgenis-theme</a> Bootstrap 4 file.</small>
+      </div>
+      <div class="form-group">
+        <label for="theme-url-legacy">Theme url (legacy)</label>
+        <input
+          id="theme-url-legacy" v-model="themeUrlLegacy"
+          type="text"
+          class="form-control" autocomplete="off"
+        >
+        <small id="emailHelp" class="form-text text-muted">Url to a customized <a href="https://github.com/molgenis/molgenis-theme">molgenis-theme</a> Bootstrap 3 file.</small>
+      </div>
     </div>
 
     <div v-else-if="selectionMethod === 'generated'">
-        <label for="navbar-style">Navbar style</label>
-        <select class="form-control" id="navbar-style" v-model="theme.variables['mg-navbar-style']">
-          <option value="brand">brand</option>
-          <option value="light">light</option>
-        </select>
+      <label for="navbar-style">Navbar style</label>
+      <select id="navbar-style" v-model="theme.variables['mg-navbar-style']" class="form-control">
+        <option value="brand">
+          brand
+        </option>
+        <option value="light">
+          light
+        </option>
+      </select>
 
-        <div class="form-group">
-          <label for="primary-color">Primary color</label><br/>
-          <input v-model="theme.variables['mg-color-primary']" type="color" name="favcolor"/>&nbsp;
-          <input v-model="theme.variables['mg-color-primary-contrast']" type="color" name="favcolor"/>
-          <small id="emailHelp" class="form-text text-muted">{{ $t('A primary branding color with with contrast color') }}</small>
-        </div>
+      <div class="form-group">
+        <label for="primary-color">Primary color</label><br>
+        <input v-model="theme.variables['mg-color-primary']" type="color" name="favcolor">&nbsp;
+        <input v-model="theme.variables['mg-color-primary-contrast']" type="color" name="favcolor">
+        <small id="emailHelp" class="form-text text-muted">{{ $t('A primary branding color with with contrast color') }}</small>
+      </div>
 
-        <div class="form-group">
-          <label for="primary-color">Secondary color</label><br/>
-          <input v-model="theme.variables['mg-color-secondary']" type="color" name="favcolor"/>&nbsp;
-          <input v-model="theme.variables['mg-color-secondary-contrast']" type="color" name="favcolor"/>
-          <small id="emailHelp" class="form-text text-muted">{{ $t('A secondary branding color with its contrast color') }}</small>
-        </div>
+      <div class="form-group">
+        <label for="primary-color">Secondary color</label><br>
+        <input v-model="theme.variables['mg-color-secondary']" type="color" name="favcolor">&nbsp;
+        <input v-model="theme.variables['mg-color-secondary-contrast']" type="color" name="favcolor">
+        <small id="emailHelp" class="form-text text-muted">{{ $t('A secondary branding color with its contrast color') }}</small>
+      </div>
     </div>
 
-    <button @click="save" class="btn btn-primary">Save</button>
-
+    <button class="btn btn-primary" @click="save">
+      Save
+    </button>
   </div>
 </template>
 
@@ -88,6 +117,15 @@ export default {
       selectedTheme: null,
       themeUrl: '',
       themeUrlLegacy: ''
+    }
+  },
+  watch: {
+    selectedTheme (newVal) {
+      this.themeUrl = `${themeRepository}/mg-${newVal}-4.css`
+      this.themeUrlLegacy = `${themeRepository}/mg-${newVal}-3.css`
+      // Changing the theme from the theme-manager will always use
+      // the current Bootstrap 4 url.
+      // document.querySelector('#bootstrap-theme').setAttribute('href', this.themeUrl)
     }
   },
   async created () {
@@ -137,15 +175,6 @@ export default {
       })
 
       location.reload()
-    }
-  },
-  watch: {
-    selectedTheme (newVal) {
-      this.themeUrl = `${themeRepository}/mg-${newVal}-4.css`
-      this.themeUrlLegacy = `${themeRepository}/mg-${newVal}-3.css`
-      // Changing the theme from the theme-manager will always use
-      // the current Bootstrap 4 url.
-      // document.querySelector('#bootstrap-theme').setAttribute('href', this.themeUrl)
     }
   }
 }
