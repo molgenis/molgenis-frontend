cd63d64823ee342226567f9a9c72eb2f65ac99a3
Jelmer Veen <j.p.veen@rug.nl>
2021-04-01 15:47:03 +0200

    feat(data-explorer): Added tabs to easily switch between record, metaData, components and editor options fix(data-explorer): Stylelint hindrances

--- packages/data-explorer/src/views/EditDetailTemplate.vue
+++ packages/data-explorer/src/views/EditDetailTemplate.vue
@@ -22,31 +22,80 @@
           </li>
         </ol>
       </nav>
-      <h1>Template editor</h1>
-      <div class="d-flex w-100 flex-wrap">
-        <div class="w-50 mr-3 align-self-stretch">
+      <h1>
+        Template editor
+      </h1>
+      <div class="my-3">
+        <b-card no-body>
+          <b-tabs pills card vertical>
+            <!-- Record -->
+            <b-tab title="Record properties" class="property-tab" active>
+              <em class="font-weight-light">Example usage: record.id</em>
+              <prop-render class="template-values mt-2" :object="record" />
+            </b-tab>
+            <!-- MetaData -->
+            <b-tab title="Metadata properties" class="property-tab">
+              <em class="font-weight-light">Example usage: metaData.id</em>
+              <prop-render class="template-values mt-2" :object="tableMeta" />
+            </b-tab>
+            <!-- Components -->
+            <b-tab title="Components" class="property-tab">
+              <table class="table table-striped">
+                <thead>
+                  <th>
+                    Component:
+                  </th>
+                  <th>Code:</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Shoppingcart Button</td>
+                    <td>&lt;shopping-cart-button :id="record.id" /&gt;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </b-tab>
+            <b-tab title="Editor options">
+              <button class="btn btn-outline-primary" @click="landscapeMode = !landscapeMode">
+                Set {{ landscapeMode ? "Portrait" : "Landscape" }}
+              </button>
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </div>
+      <div 
+        class="d-flex w-100 flex-wrap"
+        :class="{ 'flex-column': landscapeMode }"
+      >
+        <div 
+          class="mr-3 mt-3"
+          :class="[{'w-100': landscapeMode}, {'w-50': !landscapeMode}]"
+        >
           <!-- Editor -->
-          <label>Template</label>
-          <div class="border">
-            <form-component
-              id="template"
-              :options="{ showEyeButton: false }"
-              :form-fields="formFields"
-              :initial-form-data="{ template: template }"
-              @valueChange="template = $event.template"
-            />
-            <button 
-              class="btn btn-primary mb-2 ml-2" 
-              type="submit" 
-              :disabled="isSavingTemplate"
-              @click.prevent="saveTemplate"
-            >
-              Save template <font-awesome-icon v-if="isSavingTemplate" icon="spinner" spin />
-            </button>
-          </div>
+          <span class="font-weight-bold mb-1">Template</span>
+          <form-component
+            id="template"
+            class="border-top"
+            :options="{ showEyeButton: false }"
+            :form-fields="formFields"
+            :initial-form-data="{ template: template }"
+            
+            @valueChange="template = $event.template"
+          />
+          <button 
+            class="btn btn-primary mb-2 ml-2" 
+            type="submit" 
+            :disabled="isSavingTemplate"
+            @click.prevent="saveTemplate"
+          >
+            Save template <font-awesome-icon v-if="isSavingTemplate" icon="spinner" spin />
+          </button>
         </div>
-        <div class="ml-auto preview-container">
-          <label>Preview</label>
+        <div
+          class="preview-container mt-3"
+          :class="[{'w-100': landscapeMode}, {'ml-auto': !landscapeMode}]"
+        >
+          <span class="font-weight-bold mb-1">Preview</span>
           <div v-if="template" class="template-preview border">
             <custom-entity-detail
               :record="record"
@@ -55,29 +104,6 @@
           </div>
           <div v-else class="template-preview p-2 font-italic text-muted">
             No template set for current entity
-          </div>
-        </div>
-      </div>
-      <!-- Context -->
-      <div>
-        <label class="mt-3">Context</label>
-        <div class="row">
-          <div class="col-6">
-            <div class="card p-2">
-              <h5 class="card-title">
-                record
-              </h5>
-              <prop-render class="template-values" :object="record" />
-            </div>
-          </div>
-
-          <div class="col-6">
-            <div class="card p-2">
-              <h5 class="card-title">
-                metaData
-              </h5>
-              <prop-render class="template-values" :object="tableMeta" />
-            </div>
           </div>
         </div>
       </div>
@@ -107,6 +133,8 @@ export default {
   },
   data () {
     return {
+      landscapeMode: false,
+      currentTemplate: '',
       loading: true,
       isSavingTemplate: false,
       record: null,
@@ -185,7 +213,7 @@ export default {
 	</li>
  </ul>`
     }
-  }
+  },
 }
 </script>
 
@@ -196,15 +224,36 @@ export default {
     width: 48%;
   }
 
+  .template-preview {
+    max-height: 75vh;
+    overflow: auto;
+  }
+
   .template-values {
     font-family: 'Courier New', monospace;
   }
 
-  >>> label[for='template'] {
+  .property-tab {
+    height: 200px;
+    max-height: 200px;
+    overflow: auto;
+  }
+
+  /* 
+    ::v-deep is the stylelint accepted version, does the same as >>> and /deep/ 
+    >>> is the deep selector for plain css (if you don't use stylelint)
+    /deep/ is the scss selector
+  */
+
+  ::v-deep label[for='template'] {
     display: none;
   }
 
-  >>> .CodeMirror {
+  ::v-deep .CodeMirror {
     height: 25.25rem !important;
+  }
+
+  ::v-deep #template-description {
+    margin-left: 1rem;
   }
 </style>
