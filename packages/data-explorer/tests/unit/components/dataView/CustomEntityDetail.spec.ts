89ab5298e0b9904255c7e0dc37fe5316b135b392
Jelmer Veen <j.p.veen@rug.nl>
2021-04-02 14:39:22 +0200

    chore(data-explorer): added isShop accessibility in CustomEntityDetail

--- packages/data-explorer/tests/unit/components/dataView/CustomEntityDetail.spec.ts
+++ packages/data-explorer/tests/unit/components/dataView/CustomEntityDetail.spec.ts
@@ -1,7 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
 import CustomEntityDetail from '@/components/dataView/CustomEntityDetail.vue'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+
+let wrapper: any
 
 describe('CustomEntityDetail.vue', () => {
+  const state = {
+    tableSettings: {
+      isShop: false
+    }
+  }
+
+  const explorer = { state, namespaced: true }
+  const store = new Vuex.Store({ modules: { explorer } })
+
   const propsData = {
     record: {
       'col1': 'Column 1',
@@ -14,12 +30,16 @@ describe('CustomEntityDetail.vue', () => {
     },
     template: '    <h1>Hello MOLGENIS!</h1>    '
   }
-  const wrapper = shallowMount(CustomEntityDetail, { propsData })
+  wrapper = shallowMount(CustomEntityDetail, { propsData, store, localVue })
   it('exists', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
   it('should set the template', () => {
     // @ts-ignore
     expect(wrapper.vm.wrappedTemplate).toBe('<div class="custom-entity-detail-container"><h1>Hello MOLGENIS!</h1></div>')
+  })
+
+  it('can access the isShop property', () => {
+    expect(wrapper.vm.isShop).not.toBe(undefined)
   })
 })
