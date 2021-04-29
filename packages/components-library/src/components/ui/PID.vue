87d051d72d707475ca537864647b1ee091c47577
Fleur Kelpin <f.kelpin@umcg.nl>
2021-04-29 13:04:46 +0200

    refactor: emit registration event

--- packages/components-library/src/components/ui/PID.vue
+++ packages/components-library/src/components/ui/PID.vue
@@ -5,9 +5,10 @@
       <b-form @submit.prevent="submit">
         <validation-provider
           v-if="contexts.length > 1"
+          v-slot="validationContext"
           name="Context"
           rules="required"
-          v-slot="validationContext">
+        >
           <b-form-group
             label="Context:"
             label-for="context"
@@ -15,9 +16,9 @@
             <b-form-select
               v-model="contextIndex"
               :options="contextOptions"
-              :state = getValidationState(validationContext)
-              :disabled = "registration"
-            ></b-form-select>
+              :state="getValidationState(validationContext)"
+              :disabled="registration"
+            />
             <b-form-invalid-feedback>
               {{ validationContext.errors[0] }}
             </b-form-invalid-feedback>
@@ -31,12 +32,13 @@
           <b-form-input
             plaintext
             :value="context.name"
-          ></b-form-input>
+          />
         </b-form-group>
         <validation-provider
+          v-slot="validationContext"
           name="Patient ID"
           rules="required"
-          v-slot="validationContext">
+        >
           <b-form-group
             label="Patient ID:"
             description="The PID that should be pseudonymized"
@@ -45,18 +47,19 @@
             <b-form-input
               id="pid"
               v-model="pid"
-              :state = getValidationState(validationContext)
-              :disabled = "registration"
-            ></b-form-input>
+              :state="getValidationState(validationContext)"
+              :disabled="registration"
+            />
             <b-form-invalid-feedback>
               {{ validationContext.errors[0] }}
             </b-form-invalid-feedback>
           </b-form-group>
         </validation-provider>
         <validation-provider
+          v-slot="validationContext"
           name="First name"
           rules="required"
-          v-slot="validationContext">
+        >
           <b-form-group
             label="First name:"
             label-for="first"
@@ -64,18 +67,19 @@
             <b-form-input
               id="first"
               v-model="first"
-              :state = getValidationState(validationContext)
-              :disabled = "registration"
-            ></b-form-input>
+              :state="getValidationState(validationContext)"
+              :disabled="registration"
+            />
             <b-form-invalid-feedback>
               {{ validationContext.errors[0] }}
             </b-form-invalid-feedback>
           </b-form-group>
         </validation-provider>
         <validation-provider
+          v-slot="validationContext"
           name="Last name"
           rules="required"
-          v-slot="validationContext">
+        >
           <b-form-group
             label="Last name:"
             label-for="last"
@@ -83,18 +87,19 @@
             <b-form-input
               id="last"
               v-model="last"
-              :state = getValidationState(validationContext)
-              :disabled = "registration"
-            ></b-form-input>
+              :state="getValidationState(validationContext)"
+              :disabled="registration"
+            />
             <b-form-invalid-feedback>
               {{ validationContext.errors[0] }}
             </b-form-invalid-feedback>
           </b-form-group>
         </validation-provider>
         <validation-provider
+          v-slot="validationContext"
           name="Date of birth"
           rules="required|past"
-          v-slot="validationContext">
+        >
           <b-form-group
             label="Date of birth:"
             label-for="dob"
@@ -102,9 +107,9 @@
             <b-form-input
               v-model="dob"
               placeholder="yyyy-mm-dd"
-              :state = getValidationState(validationContext)
-              :disabled = "registration"
-            ></b-form-input>
+              :state="getValidationState(validationContext)"
+              :disabled="registration"
+            />
             <b-form-invalid-feedback>
               {{ validationContext.errors[0] }}
             </b-form-invalid-feedback>
@@ -112,7 +117,8 @@
         </validation-provider>
         <b-button
           type="submit"
-          :disabled="registration || !getValidationState(formValidationContext)">
+          :disabled="registration || !getValidationState(formValidationContext)"
+        >
           Register
         </b-button>
       </b-form>
@@ -122,7 +128,8 @@
       class="mt-3"
       header-bg-variant="info"
       header-text-variant="white"
-      :header="registration.psn">
+      :header="registration.psn"
+    >
       <b-card-text>
         Successfully registered pseudonym for context {{ registration.context }}.
       </b-card-text>
@@ -186,7 +193,7 @@ export default {
      * The contexts the patient can be registered in.
      */
     contexts: {
-      type: Array[{name: String, ttp: String, key: String}],
+      type: Array[{name: String, label: String, description: String, hashKey: String, ttpPublicKey: String}],
       validator: (contexts) => contexts.length > 0 
     }
   },
@@ -203,7 +210,7 @@ export default {
   },
   computed: {
     contextOptions() {
-      return this.contexts.map((context, index) => ({value: index, text: context.name}))
+      return this.contexts.map((context, index) => ({value: index, text: context.label}))
     },
     context() {
       return this.contextIndex == null ? null : this.contexts[this.contextIndex]
@@ -215,31 +222,32 @@ export default {
         SodiumUtil.populateConstants(backend)
         this.sodium = new SodiumPlus(await backend.init())
       }
-      const key = CryptographyKey.from(this.context.key, 'hex')
-      const hashed = await this.sodium.crypto_shorthash(this.pid, key)
-      const psn = hashed.toString('hex').substring(0,8).toUpperCase()
-      const phonetic = {
-        first: this.first && eudex(this.first),
-        last: this.last && eudex(this.last),
+      const key = CryptographyKey.from(this.context.hashKey, 'hex')
+      const hashedPid = await this.sodium.crypto_shorthash(this.pid, key)
+      const psn = hashedPid.toString('hex').substring(0,8).toUpperCase()
+      const hashed = {
+        first: this.first && eudex(this.first).toString(16),
+        last: this.last && eudex(this.last).toString(16),
         dob: this.dob && crc32(this.dob)
       }
-      const registration = { psn, phonetic, context: this.context.name }
-      if (this.context.ttp) {
-        const publicKey = X25519PublicKey.from(this.context.ttp, 'hex')
-        registration.crypted = {
+      const registration = { psn, hashed, context: this.context.name }
+      if (this.context.ttpPublicKey) {
+        const publicKey = X25519PublicKey.from(this.context.ttpPublicKey, 'hex')
+        registration.cryptKey = this.context.ttpPublicKey
+        registration.encrypted = {
           dob: await this.encrypt(this.dob, publicKey),
           first: await this.encrypt(this.first, publicKey),
           last: await this.encrypt(this.last, publicKey)
         }
       }
-      this.registration = registration
       /**
        * Triggers when the patient has been registered
        *
-       * @property {string} psn the pseudonym
-       * @property {string} context the name of the context
+       * @property {registration} psn the registration mutation
        */
-      this.$emit('input', registration.psn, registration.context)
+      this.$emit('input', registration)
+      // For demo purposes:
+      this.registration = registration
     },
     async encrypt (value, publicKey) {
       return (await this.sodium.crypto_box_seal(value, publicKey)).toString('hex')
@@ -258,20 +266,23 @@ Registration of patient details and generated
 ```vue
 const contexts = [
   {
-    name: "Ithaca", 
-    key: "ccbb79d11efcbcab7a95e60914ae2749",
-    ttp: "f1a70d54c453c22693767eb6e8e91229e00e833e5eff14dc32a699f258a8f62e"
+    name: "ithaca", 
+    label: "Ithaca",
+    description: "The Ithaca ERN",
+    hashKey: "ccbb79d11efcbcab7a95e60914ae2749",
+    ttpPublicKey: "f1a70d54c453c22693767eb6e8e91229e00e833e5eff14dc32a699f258a8f62e"
   }, {
-    name: "Skin",
-    key: "8f28841f22a638135688d93ec6b6170d",
-    ttp: "7b26cc4f22ac92c3b4a66efa39c401c24ba3554ace393e885db67fe33719d5b5"
+    name: "skin",
+    label: "Skin",
+    hashKey: "8f28841f22a638135688d93ec6b6170d",
+    ttpPublicKey: "7b26cc4f22ac92c3b4a66efa39c401c24ba3554ace393e885db67fe33719d5b5"
   }
 ]
-let registration = {}
+let registration = null
 
 <PID
   :contexts="contexts"
-  @input="(psn, context) => { registration = {psn, context} }"></PID>
+  @input="(value) => { registration = value }"></PID>
 
 <pre>{{ registration }}</pre>
 ```
