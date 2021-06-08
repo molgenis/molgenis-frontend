202cccaa86e0ac0f783403db1872c4f883a56e8e
Max Postema <m.e.postema@rug.nl>
2021-06-08 10:34:25 +0200

    fix: make buttons a slot

--- packages/components-library/src/components/table/TableRow.vue
+++ packages/components-library/src/components/table/TableRow.vue
@@ -9,38 +9,12 @@
       scope="col"
     >
       <input
-        v-if="isEditable" class="form-check-input"
+        v-if="isEditable" class="form-check-input mr-2"
         type="checkbox"
         :checked="isSelected" :value="id"
         @click="toggleSelectedItemsHandler(id)"
       >
-      <router-link
-        v-if="isEditable"
-        v-b-tooltip.hover.bottom
-        :title="$t('dataexplorer_row_action_edit_btn_tooltip')"
-        class="btn btn-sm text-secondary pl-2"
-        role="button"
-        :to="{ name: 'de-edit', params: { entity: tableName, dataRowId: id}, query: {}}"
-      >
-        <font-awesome-icon icon="edit" />
-      </router-link>
-      <router-link
-        v-b-tooltip.hover.bottom class="btn btn-sm text-secondary"
-        role="button"
-        :to="{ name: 'entity-detail', params: { entityType: tableName, entity: id}, query: searchRoute()}"
-      >
-        <font-awesome-icon icon="search" />
-      </router-link>
-      <button
-        v-if="isEditable"
-        v-b-tooltip.hover.bottom
-        :title="$t('dataexplorer_row_action_delete_btn_tooltip')"
-        class="btn btn-sm text-secondary"
-        role="button"
-        @click="deleteItem(id)"
-      >
-        <font-awesome-icon icon="trash" />
-      </button>
+      <slot name="edit-buttons" />
     </th>
     <td v-for="(column, index) in visibleColumns" :key="index" class="mg-data-column">
       <DataDisplayCell
@@ -121,10 +95,6 @@ export default {
     },
   },
   methods: {
-    searchRoute(){
-      if(this.$route != null) return this.$route.query;
-      return {}
-    },
     getColumnValue (name) {
       if (name in this.rowData && this.rowData[name] !== undefined) {
         return this.rowData[name]
@@ -137,9 +107,6 @@ export default {
         * @event toggleSelectedItemsHandler
         */
       this.$emit('toggleSelectedItemsHandler', id)
-    },
-    deleteItem (id) {
-      this.$eventBus.$emit('delete-item', id)
     }
   }
 }
@@ -238,11 +205,30 @@ export default {
         v-bind:is-selected="false"
         v-bind:is-editable="true"
         v-bind:show-selected="false"
-        v-bind:route="{ query: '' }" 
         v-on:toggleSelectedItemsHandler="() => { }"
     >
       <template v-slot:shopping-button>
         <button class="btn btn-primary"> Shopping-button slot </button>
+      </template>
+      <template v-slot:edit-buttons>
+        <router-link
+          class="btn btn-sm text-secondary"
+          role="button"
+        >
+          <font-awesome-icon icon="edit" />
+        </router-link>
+        <router-link
+          class="btn btn-sm text-secondary"
+          role="button"
+        >
+          <font-awesome-icon icon="search" />
+        </router-link>
+        <button
+          class="btn btn-sm text-secondary"
+          role="button"
+        >
+          <font-awesome-icon icon="trash" />
+        </button>
       </template>
     </TableRow>
   </table>
@@ -304,6 +290,28 @@ export default {
       <template v-slot:shopping-button>
         <button class="btn btn-primary"> Shopping-button slot </button>
       </template>
+      <template v-slot:edit-buttons>
+        <router-link
+          class="btn btn-sm text-secondary"
+          role="button"
+          v-if="iseditable"
+        >
+          <font-awesome-icon icon="edit" />
+        </router-link>
+        <router-link
+          class="btn btn-sm text-secondary"
+          role="button"
+        >
+          <font-awesome-icon icon="search" />
+        </router-link>
+        <button
+          class="btn btn-sm text-secondary"
+          v-if="iseditable"
+          role="button"
+        >
+          <font-awesome-icon icon="trash" />
+        </button>
+      </template>      
     </TableRow>
   </table>
   <hr/>
